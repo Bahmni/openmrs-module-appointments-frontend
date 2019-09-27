eb09e5619a5457f138c7c26b888ad006213ea052
Sneha Bagri <bsneha@thoughtworks.com>
2019-09-27 18:57:06 +0530

    Sneha Bagri, Bindu | Create a component to show the new appointment editor on add new appointment

---
+++ ui/react-components/containers/AddAppointmentContainer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { AppointmentEditor } from "../AppointmentEditor/AppointmentEditor.jsx";
+
+// TODO : need to add connection to redux
+const AddAppointmentContainer = () => {
+    return (<AppointmentEditor/>);
+}
+
+export default AddAppointmentContainer;
