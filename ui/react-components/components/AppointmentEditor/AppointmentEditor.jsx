bdaa49c0ca9c8a53d17009a41c4baad56960c3ef
Sneha Bagri <bsneha@thoughtworks.com>
2019-10-07 10:35:42 +0530

    Sneha| MOBN-612| Callback to get the selected patient value

--- ui/react-components/components/AppointmentEditor/AppointmentEditor.jsx
+++ ui/react-components/components/AppointmentEditor/AppointmentEditor.jsx
@@ -1,14 +1,16 @@
-import React, {Fragment} from "react";
+import React, {Fragment, useState} from "react";
 import classNames from 'classnames';
 import {appointmenteditor, patientSearch} from './AppointmentEditor.module.scss';
 import PatientSearch from "../PatientSearch/PatientSearch.jsx";
 
 export const AppointmentEditor = () => {
+    const [patient, setPatient] = useState();
+    console.log(patient)
     return (<Fragment>
         <div data-testid="appointment-editor"
              className={classNames(appointmenteditor)}>
             <div className={classNames(patientSearch)}>
-                <PatientSearch/>
+                <PatientSearch onChange={(optionSelected) => setPatient(optionSelected.value) }/>
             </div>
         </div>
     </Fragment>);
