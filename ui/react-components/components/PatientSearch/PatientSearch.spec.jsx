0d9d395885c598e90ef938bb3c12e35d123ea917
Sneha Bagri <bsneha@thoughtworks.com>
2019-10-07 12:30:28 +0530

    Sneha| MOBN-612| Tests for patient search component

--- ui/react-components/components/PatientSearch/PatientSearch.spec.jsx
+++ ui/react-components/components/PatientSearch/PatientSearch.spec.jsx
@@ -7,8 +7,17 @@ import {renderWithReactIntl} from '../../utils/TestUtil';
 
 jest.mock('../../api/patientApi');
 jest.mock('../../utils/CookieUtil');
+const patientApi = require('../../api/patientApi');
+let getPatientByLocationSpy;
 
 describe('Patient Search', () => {
+    beforeEach(() => {
+        getPatientByLocationSpy = jest.spyOn(patientApi, 'getPatientsByLocation');
+    });
+    afterEach(() => {
+        getPatientByLocationSpy.mockRestore();
+    });
+
     it('should allow user to search and select a patient', async () => {
         const targetPatient = '9DEC74AB 9DEC74B7 (IQ1110)';
         const {container, getByText} = renderWithReactIntl(<PatientSearch onChange={jest.fn()}/>);
@@ -26,7 +35,46 @@ describe('Patient Search', () => {
                     '.react-select__single-value'
                 ))
         );
+        expect(getPatientByLocationSpy).toHaveBeenCalled();
         expect(singleValue).toHaveTextContent(targetPatient);
+    });
+
+    it('should not search for patients when less than 3 characters are entered', async () => {
+        const targetPatient = '9DEC74AB 9DEC74B7 (IQ1110)';
+        const {container, getByText} = renderWithReactIntl(<PatientSearch onChange={jest.fn()}/>);
+        const inputBox = container.querySelector('.react-select__input input');
+        fireEvent.change(inputBox, { target: { value: "ab" } });
+        await waitForElement(
+            () => (container.querySelector('.react-select__menu'))
+        );
+        expect(getPatientByLocationSpy).not.toHaveBeenCalled();
+    });
+
+    it('should display placeholder as "Patient ID"', async () => {
+        const targetPatient = '9DEC74AB 9DEC74B7 (IQ1110)';
+        const {container, getByText} = renderWithReactIntl(<PatientSearch onChange={jest.fn()}/>);
+        getByText('Patient ID');
+    });
+
+    it('should call onChnage when option is selected', async () => {
+        const targetPatient = '9DEC74AB 9DEC74B7 (IQ1110)';
+        const onChangeSpy = jest.fn();
+        const {container, getByText} = renderWithReactIntl(<PatientSearch onChange={onChangeSpy}/>);
+        const inputBox = container.querySelector('.react-select__input input');
+        fireEvent.change(inputBox, { target: { value: "abc" } });
+        await waitForElement(
+            () => (container.querySelector('.react-select__menu'))
+        );
+        const option = getByText(targetPatient);
+        fireEvent.click(option);
+        let singleValue;
+        await waitForElement(
+            () =>
+                (singleValue = container.querySelector(
+                    '.react-select__single-value'
+                ))
+        );
+        expect(onChangeSpy).toHaveBeenCalledTimes(1);
     });
 });
 
