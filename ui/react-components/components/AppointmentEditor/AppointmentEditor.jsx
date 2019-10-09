73b496a4edfb0f717511a4e6207aa4e74e806a07
Alekhya <alekhyayalla23@gmail.com>
2019-10-09 15:56:04 +0530

    Vinay, Alekhya | MOBN-613 | Add service component on appointment editor

--- ui/react-components/components/AppointmentEditor/AppointmentEditor.jsx
+++ ui/react-components/components/AppointmentEditor/AppointmentEditor.jsx
@@ -2,14 +2,17 @@ import React, {Fragment, useState} from "react";
 import classNames from 'classnames';
 import {appointmenteditor, patientSearch} from './AppointmentEditor.module.scss';
 import PatientSearch from "../PatientSearch/PatientSearch.jsx";
+import ServiceSearch from "../Service/ServiceSearch.jsx";
 
 export const AppointmentEditor = () => {
     const [patient, setPatient] = useState();
+    const [serviceUuid, setServiceUuid] = useState('');
     return (<Fragment>
         <div data-testid="appointment-editor"
              className={classNames(appointmenteditor)}>
             <div className={classNames(patientSearch)}>
                 <PatientSearch onChange={(optionSelected) => setPatient(optionSelected.value) }/>
+                <ServiceSearch onChange={(optionSelected) => setServiceUuid(optionSelected.value)}/>
             </div>
         </div>
     </Fragment>);
