23f99eaa2aa21dbef18464e9dca509f9b053c2ac
Alekhya <alekhyayalla23@gmail.com>
2019-10-09 15:56:04 +0530

    Vinay, Alekhya | MOBN-613 | Add service component on appointment editor

---
+++ ui/react-components/components/Service/ServiceSearch.jsx
@@ -0,0 +1,43 @@
+import React, {useState, useEffect} from 'react';
+import {getAllServices} from "../../api/serviceApi";
+import Dropdown from "../Dropdown/Dropdown.jsx";
+import PropTypes from "prop-types";
+
+const ServiceSearch = (props) => {
+
+    const [placeHolder] = useState("Select your service");
+    const [services, setServices] = useState([]);
+
+    useEffect(() => { setServices(loadServices()) },[]);
+
+
+    const loadServices = async () => {
+        const services = await getAllServices();
+        setServices(createDropdownOptions(services));
+    };
+
+    const createDropdownOptions = (results) => {
+        const options = [];
+        _.forEach(results, function (service) {
+            options.push({
+                value: service.uuid,
+                label: service.name
+            })
+        });
+        return options;
+    };
+
+    return (
+        <Dropdown
+            options={Object.values(services)}
+            placeholder={placeHolder}
+            onChange={props.onChange}
+        />
+    );
+};
+
+ServiceSearch.propTypes = {
+    onChange: PropTypes.func.isRequired
+};
+
+export default ServiceSearch;
