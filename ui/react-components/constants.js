3a3bb408d38da8c442bbd535113dc576016e7ca5
Alekhya <alekhyayalla23@gmail.com>
2019-10-09 15:56:04 +0530

    Vinay, Alekhya | MOBN-613 | Add service component on appointment editor

--- ui/react-components/constants.js
+++ ui/react-components/constants.js
@@ -2,7 +2,9 @@ const hostUrl = localStorage.getItem('host') ? ("https://" + localStorage.getIte
 const restWestV1 = `${hostUrl}/openmrs/ws/rest/v1`;
 const bahmniCore = `${restWestV1}/bahmnicore`;
 
+export const appointmentService = `${restWestV1}/appointmentService`;
 export const searchPatientUrl = `${bahmniCore}/search/patient`;
+export const servicesDefaultUrl = `${appointmentService}/all/default`;
 
 export const appName = 'appointments';
 
