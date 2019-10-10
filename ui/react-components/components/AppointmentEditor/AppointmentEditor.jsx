76b9b58958fed45a2b99cbe8da92a4a5eb881248
Alekhya <alekhyayalla23@gmail.com>
2019-10-10 22:05:13 +0530

    Sowmika, Vinay | MOBN - 613 | Add ErrorMessage component

--- ui/react-components/components/AppointmentEditor/AppointmentEditor.jsx
+++ ui/react-components/components/AppointmentEditor/AppointmentEditor.jsx
@@ -1,12 +1,18 @@
 import React, {Fragment, useState} from "react";
 import classNames from 'classnames';
-import {appointmenteditor, searchFieldsContainerRight, searchFieldsContainerLeft, searchFieldsContainer} from './AppointmentEditor.module.scss';
+import {
+    appointmenteditor,
+    searchFieldsContainerRight,
+    searchFieldsContainerLeft,
+    searchFieldsContainer
+} from './AppointmentEditor.module.scss';
 import PatientSearch from "../PatientSearch/PatientSearch.jsx";
 import ServiceSearch from "../Service/ServiceSearch.jsx";
 import ServiceTypeSearch from "../Service/ServiceTypeSearch.jsx";
 import ProviderSearch from "../Provider/ProviderSearch.jsx";
 import LocationSearch from "../Location/LocationSearch.jsx";
 import SpecialitySearch from "../Speciality/SpecialitySearch.jsx";
+import ErrorMessage from "../ErrorMessage/ErrorMessage.jsx";
 
 export const AppointmentEditor = () => {
     const [patient, setPatient] = useState();
@@ -16,15 +22,28 @@ export const AppointmentEditor = () => {
     const [location, setLocation] = useState('');
     const [speciality, setSpeciality] = useState('');
 
+
     return (<Fragment>
         <div data-testid="appointment-editor" className={classNames(appointmenteditor)}>
             <div className={classNames(searchFieldsContainer)}>
                 <div className={classNames(searchFieldsContainerLeft)}>
-                    <PatientSearch onChange={(optionSelected) => setPatient(optionSelected.value)}/>
-                    <ServiceSearch onChange={(optionSelected) => setService(optionSelected.value)}/>
-                    <ServiceTypeSearch onChange={(optionSelected) => setServiceType(optionSelected.value)} serviceUuid={service} />
-                    <SpecialitySearch onChange={(optionSelected) => setSpeciality(optionSelected.value)}/>
-                    <LocationSearch onChange={(optionSelected) => setLocation(optionSelected.value)}/>
+                    <div>
+                        <PatientSearch onChange={(optionSelected) => setPatient(optionSelected.value)}/>
+                    </div>
+                    <div>
+                        <ServiceSearch onChange={(optionSelected) => setService(optionSelected.value)}/>
+                        <ErrorMessage />
+                    </div>
+                    <div>
+                        <ServiceTypeSearch onChange={(optionSelected) => setServiceType(optionSelected.value)}
+                                           serviceUuid={service}/>
+                    </div>
+                    <div>
+                        <SpecialitySearch onChange={(optionSelected) => setSpeciality(optionSelected.value)}/>
+                    </div>
+                    <div>
+                        <LocationSearch onChange={(optionSelected) => setLocation(optionSelected.value)}/>
+                    </div>
                 </div>
                 <div className={classNames(searchFieldsContainerRight)}>
                     <ProviderSearch onChange={selectedProviders => setProviders(selectedProviders)}/>
