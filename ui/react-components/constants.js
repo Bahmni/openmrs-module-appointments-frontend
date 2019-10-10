ab98664ce4a0764eb573999ce97d04b541e7a36c
sowmika <sowmika148@gmail.com>
2019-10-10 18:29:12 +0530

    Sowmika | MOBN-613 | Add Location search component

--- ui/react-components/constants.js
+++ ui/react-components/constants.js
@@ -9,8 +9,10 @@ export const providerUrl = `${restWestV1}/provider`;
 export const providerParams = `v=custom:(display,person,uuid,retired,attributes:(attributeType:(display),value,voided))`;
 export const availableForAppointments = "Available for appointments";
 export const locationUrl = `${restWestV1}/location`;
+export const specialityUrl = `${restWestV1}/speciality/all`;
 
 export const appName = 'appointments';
 
 export const BAHMNI_CONFIG_URL = `${hostUrl}/bahmni_config/openmrs/apps`;
 export const IMPLEMENTATION_CONFIG_URL = `${hostUrl}/implementation_config/openmrs/apps`;
+export const locationTagName = 'Appointment Location';
