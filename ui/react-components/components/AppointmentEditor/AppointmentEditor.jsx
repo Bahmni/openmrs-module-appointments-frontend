37b8b90916a2f87508a4a2a7d998fa9ee4b0e073
Alekhya <alekhyayalla23@gmail.com>
2019-10-10 17:29:15 +0530

    Vinay, Alekhya | MOBN-613 | Add tests for service type component

--- ui/react-components/components/AppointmentEditor/AppointmentEditor.jsx
+++ ui/react-components/components/AppointmentEditor/AppointmentEditor.jsx
@@ -1,6 +1,11 @@
 import React, {Fragment, useState} from "react";
 import classNames from 'classnames';
-import {appointmenteditor, patientServiceSearch, providerSearch, searchFieldsContainer} from './AppointmentEditor.module.scss';
+import {
+    appointmenteditor,
+    patientServiceSearch,
+    providerSearch,
+    searchFieldsContainer
+} from './AppointmentEditor.module.scss';
 import PatientSearch from "../PatientSearch/PatientSearch.jsx";
 import ServiceSearch from "../Service/ServiceSearch.jsx";
 import ServiceTypeSearch from "../Service/ServiceTypeSearch.jsx";
@@ -9,15 +14,16 @@ import ProviderSearch from "../Provider/ProviderSearch.jsx";
 export const AppointmentEditor = () => {
     const [patient, setPatient] = useState();
     const [providers, setProviders] = useState([]);
-    const [serviceUuid, setServiceUuid] = useState('');
+    const [service, setService] = useState('');
+    const [serviceType, setServiceType] = useState('');
 
     return (<Fragment>
         <div data-testid="appointment-editor" className={classNames(appointmenteditor)}>
             <div className={classNames(searchFieldsContainer)}>
                 <div className={classNames(patientServiceSearch)}>
                     <PatientSearch onChange={(optionSelected) => setPatient(optionSelected.value) }/>
-                    <ServiceSearch onChange={(optionSelected) => setServiceUuid(optionSelected.value)}/>
-                    <ServiceTypeSearch serviceUuid={serviceUuid} />
+                    <ServiceSearch onChange={(optionSelected) => setService(optionSelected.value)}/>
+                    <ServiceTypeSearch onChange={(optionSelected) => setServiceType(optionSelected.value)} serviceUuid={service} />
                 </div>
                 <div className={classNames(providerSearch)}>
                     <ProviderSearch onChange={selectedProviders => setProviders(selectedProviders)}/>
