73079b39fe51f873c308d79d96f5b5123251947a
sowmika <sowmika148@gmail.com>
2019-10-10 16:12:42 +0530

    Sowmika | MOBN-613 | Add Speciality api to fetch all specialities

---
+++ ui/react-components/api/specialityApi.spec.js
@@ -0,0 +1,32 @@
+import mockAxios from "jest-mock-axios";
+import {specialityUrl} from "../constants";
+import {getAllSpecialities} from "./specialityApi";
+
+afterEach(() => {
+    mockAxios.reset();
+});
+
+describe('Speciality service', () => {
+    it('should return specialities', async () => {
+        let mockResponse = [
+            {
+                "name": "SpecialityOne",
+                "uuid": "SpecialityOneUuid"
+            },
+            {
+                "name": "SpecialityTwo",
+                "uuid": "SpecialityTwoUuid"
+            }
+        ];
+        mockAxios.get.mockImplementationOnce(() =>
+            Promise.resolve({
+                data: mockResponse
+            })
+        );
+
+        let specialities = await getAllSpecialities();
+
+        expect(mockAxios.get).toHaveBeenCalledWith(specialityUrl);
+        expect(specialities).toEqual(mockResponse);
+    });
+});
