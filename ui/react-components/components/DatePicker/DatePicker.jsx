21a52a3f3377b5cf33f7d8f391eb524c8ce20189
Siva Rachakonda <shiva.rachakonda139@gmail.com>
2019-10-13 14:00:41 +0530

    Siva | MOBN-613 | Update Label as stand-alone component

---
+++ ui/react-components/components/DatePicker/DatePicker.jsx
@@ -0,0 +1,37 @@
+import React, { Text } from 'react';
+import Calendar from 'rc-calendar';
+import 'rc-calendar/assets/index.css';
+import { appointmentDatePicker } from './DatePicker.module.scss';
+import PropTypes from 'prop-types';
+
+const AppointmentDatePicker = (props) => {
+
+    const disablePastDates = (current) => {
+        if (!current) {
+            return false;
+        }
+        const date = new Date();
+        date.setHours(0);
+        date.setMinutes(0);
+        date.setSeconds(0);
+        return current.valueOf() < date.valueOf();
+    }
+
+    return (
+        <div>
+            <Calendar
+                showToday={false}
+                showOk={false}
+                onChange={props.onChange}
+                disabledDate={disablePastDates}
+                className={appointmentDatePicker}
+            />
+        </div>
+    );
+};
+
+AppointmentDatePicker.propTypes = {
+    onChange: PropTypes.func.isRequired
+};
+
+export default AppointmentDatePicker;
