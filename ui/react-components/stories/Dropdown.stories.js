a218c2d17207da0d3ce438325d5960ac63dfa61c
Ashwini Manoj <ashwinimanoj@gmail.com>
2019-10-15 22:43:28 +0530

    Add dropdown stories for react-storybook

---
+++ ui/react-components/stories/Dropdown.stories.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Dropdown from '../components/Dropdown/Dropdown';
+
+export default { title: 'Dropdown' };
+
+const colourOptions = [
+    {value: 'ocean', label: 'Ocean'},
+    {value: 'blue', label: 'Blue'},
+    {value: 'orange', label: 'Orange'},
+    {value: 'yellow', label: 'Yellow'},
+    {value: 'green', label: 'Green'},
+    {value: 'forest', label: 'Forest'},
+    {value: 'slate', label: 'Slate'},
+    {value: 'silver', label: 'Silver'}
+];
+
+export const withLabel = () => <Dropdown placeholder="Sample placeholder"></Dropdown>;
+
+export const withOptions = () => <Dropdown placeholder="Select color" options={colourOptions}></Dropdown>;
