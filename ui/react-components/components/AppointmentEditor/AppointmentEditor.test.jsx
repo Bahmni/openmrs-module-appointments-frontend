14e8054ab93cdb697409e8f0326211c00d769516
sowmika <sowmika148@gmail.com>
2019-10-15 12:53:30 +0530

    Sowmika, Alekhya | MOBN-613 | Add tests for saveAppointment method call in appointment editor

--- ui/react-components/components/AppointmentEditor/AppointmentEditor.test.jsx
+++ ui/react-components/components/AppointmentEditor/AppointmentEditor.test.jsx
@@ -1,8 +1,30 @@
 import React from "react";
 import AppointmentEditor from './AppointmentEditor.jsx';
 import {renderWithReactIntl} from '../../utils/TestUtil';
+import {fireEvent, waitForElement} from "@testing-library/react";
+import * as save from './AppointmentEditorService.js';
+import ServiceSearch from "../Service/ServiceSearch";
+import moment from "moment";
+
+jest.mock('../../api/patientApi');
+jest.mock('../../api/serviceApi');
+jest.mock('../../utils/CookieUtil');
+const patientApi = require('../../api/patientApi');
+const serviceApi = require('../../api/serviceApi');
+let getPatientByLocationSpy;
+let getAllServicesSpy;
 
 describe('Appointment Editor', () => {
+
+    beforeEach(() => {
+        getPatientByLocationSpy = jest.spyOn(patientApi, 'getPatientsByLocation');
+        getAllServicesSpy = jest.spyOn(serviceApi, 'getAllServices');
+    });
+    afterEach(() => {
+        getPatientByLocationSpy.mockRestore();
+        getAllServicesSpy.mockRestore();
+    });
+
     it('should render an editor', () => {
         const {container} = renderWithReactIntl(<AppointmentEditor/>);
         expect(container.hasChildNodes()).toBeTruthy();
@@ -43,8 +65,6 @@ describe('Appointment Editor', () => {
         expect(getAllByTestId('select').length).toBe(5);
     });
 
-    //TODO : Need to write tests for checkAndSave
-
     it('should render AppointmentEditorFooter', function() {
        const {getByTestId, container} = renderWithReactIntl(<AppointmentEditor/>);
         expect(container.querySelector('.footer')).not.toBeNull();
@@ -56,6 +76,71 @@ describe('Appointment Editor', () => {
     it('should render AppointmentDatePicker', function () {
         const {getByTestId} = renderWithReactIntl(<AppointmentEditor/>);
         expect(getByTestId('datePicker')).not.toBeNull()
+    });
+
+    it('should display error messages when checkAndSave is clicked and required fields are not selected', () => {
+        const {getByText, getAllByText} = renderWithReactIntl(<AppointmentEditor/>);
+        const button = getByText('Check and Save');
+        const saveAppointmentSpy = jest.spyOn(save, 'saveAppointment');
+        fireEvent.click(button);
+        getByText('Please select patient');
+        getByText('Please select service');
+        getByText('Please select date');
+        const timeError = getAllByText('Please select time');
+        expect(timeError.length).toBe(2);
+        expect(saveAppointmentSpy).not.toHaveBeenCalled();
+    });
+
+    it('should display time error message when time is not selected and remaining fields are selected ', async () => {
+        const {container, getByText, queryByText, getAllByTitle, getAllByText} = renderWithReactIntl(<AppointmentEditor/>);
+
+        //select patient
+        const targetPatient = '9DEC74AB 9DEC74B7 (IQ1110)';
+        const inputBox = container.querySelector('.react-select__input input');
+        fireEvent.change(inputBox, { target: { value: "abc" } });
+        await waitForElement(
+            () => (container.querySelector('.react-select__menu'))
+        );
+        const option = getByText(targetPatient);
+        fireEvent.click(option);
+        let singleValue;
+        await waitForElement(
+            () =>
+                (singleValue = container.querySelector(
+                    '.react-select__single-value'
+                ))
+        );
+
+        //select service
+        const targetService = 'Physiotherapy OPD';
+        const inputBoxService = container.querySelectorAll('.react-select__input input')[1];
+        fireEvent.change(inputBoxService, { target: { value: "Phy" } });
+        await waitForElement(() => (container.querySelector('.react-select__menu')));
+        const optionService = getByText(targetService);
+        fireEvent.click(optionService);
+        let singleValueService;
+        await waitForElement(
+            () =>
+                (singleValueService = container.querySelector(
+                    '.react-select__single-value'
+                ))
+        );
+
+        //select date
+        const getCellByTitle = (getAllByTitle, title) => {
+            const querySelector = getAllByTitle(title);
+            return querySelector[0].children[0];
+        };
+        const tomorrow = moment().add(1, "days").format("MMMM D, YYYY");
+        const dateCell = getCellByTitle(getAllByTitle, tomorrow);
+        fireEvent.click(dateCell);
+
+        fireEvent.click(getByText('Check and Save'));
+
+        expect(queryByText('Please select patient')).toBeNull();
+        expect(queryByText('Please select service')).toBeNull();
+        expect(queryByText('Please select date')).toBeNull();
+        expect(getAllByText('Please select time').length).toBe(2);
     })
 });
 
