5184eea0427c1e45c2e79b3e976de6357db46caa
tbindu <tbindu@thoughtworks.com>
2019-10-15 12:20:28 +0530

    Bindu | MOBN-613 | Added testcases for Label and Notes components

---
+++ ui/react-components/components/AppointmentNotes/AppointmentNotes.spec.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '@testing-library/jest-dom/extend-expect';
+import AppointmentNotes from './AppointmentNotes.jsx';
+import {render, fireEvent } from '@testing-library/react';
+
+describe('Appointment Notes', () => {
+   it('should call onChange when notes has entered',() => {
+       const onChangeSpy =  jest.fn();
+       const {container, getByText} = render(<AppointmentNotes onChange={onChangeSpy}/>);
+       const inputBox = container.querySelector('.notes');
+       fireEvent.change(inputBox, {target:{value:'someNotes'}});
+       expect(onChangeSpy.mock.calls.length).toBe(1);
+   })
+});
