84f488acb188f57cbb8f0976a980a07a6ac8a093
sowmika <sowmika148@gmail.com>
2019-10-16 20:17:40 +0530

    Sowmika, Vineela | MOBN-736 | Add cancelConfirmation component and fix intl warnings in tests

--- ui/react-components/components/CustomPopup/CustomPopup.spec.jsx
+++ ui/react-components/components/CustomPopup/CustomPopup.spec.jsx
@@ -1,12 +1,13 @@
 import CustomPopup from "./CustomPopup";
 import React from 'react';
 import {render, fireEvent} from '@testing-library/react'
-
+import {TestUtil} from "./TestUtil.jsx";
 
 describe('Custom popup', () => {
     it('should display the given text provided in popup content on click of trigger component', () => {
         const triggerComponent = <button>Button</button>;
-        const {getByText} = render(<CustomPopup triggerComponent={triggerComponent} popupContent={<div>Text</div>}/>);
+        const {getByText} = render(<CustomPopup triggerComponent={triggerComponent}
+                                                           popupContent={React.cloneElement(<TestUtil/>, {close: jest.fn()})}/>);
         fireEvent.click(getByText('Button'));
         getByText('Text');
     });
