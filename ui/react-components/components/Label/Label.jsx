8da9b46db3bae82f8317d29330c8ab96f30d4328
Alekhya <alekhyayalla23@gmail.com>
2019-10-16 11:27:13 +0530

    Alekhya | MOBN-613 | Use formatted message component for internationalisation

--- ui/react-components/components/Label/Label.jsx
+++ ui/react-components/components/Label/Label.jsx
@@ -1,21 +1,17 @@
 import React from 'react';
-import { injectIntl } from 'react-intl';
+import {FormattedMessage} from 'react-intl';
 import PropTypes from 'prop-types';
 
 const Label = props => {
-    const { intl, translationKey, defaultValue } = props;
-    const labelValue = intl.formatMessage({
-        id: translationKey, defaultMessage: defaultValue
-    });
+    const { translationKey, defaultValue } = props;
     return (
-        <label><b>{labelValue}</b></label>
+        <label><b><FormattedMessage id={translationKey} defaultMessage={defaultValue}/></b></label>
     )
 }
 
 Label.propTypes = {
-    intl: PropTypes.object.isRequired,
     translationKey: PropTypes.string.isRequired,
     defaultValue: PropTypes.string.isRequired
 };
 
-export default injectIntl(Label);
+export default Label;
