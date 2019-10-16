246a2e5ede103bb002a93a5f9ee42b7a21a21d33
sowmika <sowmika148@gmail.com>
2019-10-16 20:17:40 +0530

    Sowmika, Vineela | MOBN-736 | Add cancelConfirmation component and fix intl warnings in tests

--- ui/react-components/components/AppointmentEditorFooter/AppointmentEditorFooter.jsx
+++ ui/react-components/components/AppointmentEditorFooter/AppointmentEditorFooter.jsx
@@ -1,23 +1,34 @@
 import classNames from "classnames";
-import {button, footer, footerElements, save} from "../AppointmentEditorFooter/AppointmentEditorFooter.module.scss";
+import {
+    button,
+    footer,
+    footerElements,
+    cancelPopup,
+    save
+} from "../AppointmentEditorFooter/AppointmentEditorFooter.module.scss";
 import React from "react";
 import PropTypes from "prop-types";
 import {FormattedMessage} from "react-intl";
+import CancelConfirmation from "../CancelConfirmation/CancelConfirmation.jsx";
+import CustomPopup from "../CustomPopup/CustomPopup.jsx";
+
 
 const AppointmentEditorFooter = props => {
 
     const {checkAndSave} = props;
 
+    const popupContent = <CancelConfirmation/>;
+
+    const cancelButton = <button className={classNames(button)}>
+                            <i className={classNames("fa", "fa-times")}/>
+                            <span><FormattedMessage id={'APPOINTMENT_CREATE_CANCEL'} defaultMessage={'Cancel'}/></span>
+                        </button>;
+
     return (
         <div className={classNames(footer)}>
             <div className={classNames(footerElements)}>
-                <button className={classNames(button)} data-testid="cancel">
-                    <i className={classNames("fa", "fa-times")}/>
-                    <span>
-                        <FormattedMessage id={'APPOINTMENT_CREATE_CANCEL'} defaultMessage={'Cancel'}/>
-                    </span>
-                </button>
-                <button className={classNames(button, save)} onClick={checkAndSave} data-testid="check-and-save">
+                <CustomPopup triggerComponent={cancelButton} popupContent={popupContent}/>
+                <button className={classNames(button, save)} onClick={checkAndSave}>
                     <i className={classNames("fa", "fa-check")}/>
                     <span>
                         <FormattedMessage id={'APPOINTMENT_CREATE_CHECK_AND_SAVE'} defaultMessage={'Check and Save'}/>
