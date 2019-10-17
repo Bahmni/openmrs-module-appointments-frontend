e12a5b0201b68851a57be68460148c3d128133e6
MD Dubey <mddubey409@gmail.com>
2019-10-17 19:07:14 +0530

    Dubey | AP-5 | Change the github release script to provide username/orgname

--- github-release.rb
+++ github-release.rb
@@ -8,34 +8,49 @@ def readVersion
   data['version']
 end
 
-def create_release(token, tag_name)
-  uri = URI('https://api.github.com/repos/Bahmni/openmrs-module-appointment-frontend/releases')
+def create_release(token, tag_name, username)
+  uri = URI.parse("https://api.github.com/repos/#{username}/openmrs-module-appointment-frontend/releases")
   data = {"tag_name" => tag_name, "name" => tag_name}.to_json
   headers = {"Authorization" => "token #{token}", "Content-Type" => "application/json"}
-  response = Net::HTTP.post(uri, data, headers)
+
+  http = Net::HTTP.new(uri.host, uri.port)
+  http.use_ssl = true
+  request = Net::HTTP::Post.new(uri.request_uri, headers)
+  request.body = data
+  response = http.request(request)
   puts "Create Release Request Response Status #{response.code}"
   JSON.parse(response.body)['id']
 end
 
-def upload_asset(token, name, file, id)
-  uri = URI("https://uploads.github.com/repos/Bahmni/openmrs-module-appointment-frontend/releases/#{id}/assets?name=#{name}")
+def upload_asset(token, name, file, id, username)
+  uri = URI.parse("https://uploads.github.com/repos/#{username}/openmrs-module-appointment-frontend/releases/#{id}/assets?name=#{name}")
   data = File.read(file)
   headers = {"Authorization" => "token #{token}", "Content-Type" => "application/octet-stream"}
 
-  response = Net::HTTP.post(uri, data, headers)
+  http = Net::HTTP.new(uri.host, uri.port)
+  http.use_ssl = true
+  request = Net::HTTP::Post.new(uri.request_uri, headers)
+  request.body = data
+  response = http.request(request)
   puts "Upload Asset Request Response Status #{response.code}"
 end
 
 
 def main
+  if ARGV.length < 1
+    puts "Usage: ruby github-release.rb <username>"
+    exit
+  end
+
+  username = ARGV[0]
   token = ENV['GH_AUTH_TOKEN']
   version = readVersion
   name = "openmrs-module-appointment-frontend.zip"
   file = File.join(Dir.pwd, name)
 
-  release_id = create_release(token, version)
+  release_id = create_release(token, version, username)
 
-  upload_asset(token,name, file, release_id)
+  upload_asset(token,name, file, release_id, username)
 end
 
 main
