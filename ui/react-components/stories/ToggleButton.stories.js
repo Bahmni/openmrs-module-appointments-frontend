5878e015e4fa2beee54a0c381face50f493a82ef
Ashwini Manoj <ashwinimanoj@gmail.com>
2019-10-20 14:11:14 +0530

    Add ToggleButton story

---
+++ ui/react-components/stories/ToggleButton.stories.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ToggleButton from '../components/ToggleButton/ToggleButton';
+
+export default { title: 'ToggleButton' };
+
+export const basicToggleButton = () => (<ToggleButton></ToggleButton>);
