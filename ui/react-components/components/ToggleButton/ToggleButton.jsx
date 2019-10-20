bc63052ebba9a52f0334c94b37a2528c487639c0
Ashwini Manoj <ashwinimanoj@gmail.com>
2019-10-20 14:12:09 +0530

    Update toggle button css, remove label prop from toggle button

--- ui/react-components/components/ToggleButton/ToggleButton.jsx
+++ ui/react-components/components/ToggleButton/ToggleButton.jsx
@@ -1,21 +1,28 @@
-import React, {Component} from "react";
-import { injectIntl } from "react-intl";
+import React, {Component, Fragment} from "react";
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import './ToggleButton.module.scss';
 
 const ToggleButton = (props) => {
-    const {label, disabled} = props;
+    const {disabled} = props;
+
     return (
-            <label className={"toggle-btn-switch"}>
-                <input type="checkbox" disabled={disabled}/>
-                <span className={"toggle-btn-slider"} />
-                {label}
+        <Fragment>
+            <input
+                className={classNames("toggle-btn-checkbox")}
+                type="checkbox"
+                id='toggle-btn-checkbox'
+                disabled={disabled}/>
+            <label className={classNames("toggle-btn-label")} htmlFor='toggle-btn-checkbox'>
+                <span className={classNames("toggle-btn-slider")}>
+                </span>
             </label>
+        </Fragment>
     );
 };
 
-export default injectIntl(ToggleButton);
+export default ToggleButton;
 
 ToggleButton.propTypes = {
-    label: PropTypes.string,
     disabled: PropTypes.bool
 };
--- ui/react-components/components/ToggleButton/ToggleButton.test.jsx
+++ ui/react-components/components/ToggleButton/ToggleButton.test.jsx
@@ -1,18 +1,19 @@
 import React from "react";
-import { renderWithReactIntl } from "../../utils/TestUtil";
 import ToggleButton from './ToggleButton';
+import { render } from "@testing-library/react";
 
 describe('ToggleButton', () => {
-    it('should render label', () => {
-        const label = 'My Label';
-        const {getByText, container} = renderWithReactIntl(<ToggleButton label={label}/>);
-        getByText(label);
-        expect(container.querySelector('input').disabled).toBeFalsy();
+    it('should render toggle button', () => {
+        const {container} = render(<ToggleButton/>);
+        const toggleBtnElement = container.querySelector('.toggle-btn-checkbox');
+        expect(toggleBtnElement).not.toBeNull();
+        expect(toggleBtnElement.disabled).toBeFalsy();
     });
 
     it('should render disabled toggle button', () => {
-        const {container} = renderWithReactIntl(<ToggleButton disabled/>);
-        expect(container.querySelector('input').disabled).toBeTruthy();
+        const {container} = render(<ToggleButton disabled/>);
+        const toggleBtnElement = container.querySelector('.toggle-btn-checkbox');
+        expect(toggleBtnElement.disabled).toBeTruthy();
     });
 });
 
