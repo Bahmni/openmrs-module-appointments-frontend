a25f9b0ec33fc9e8857bc1c99106fdef1fd37c29
tbindu <tbindu@thoughtworks.com>
2019-10-21 15:58:38 +0530

    Alekhya, Bindu | MOBN-620 | Add RecurrenceTypeRadioGroup and frequency selector

---
+++ ui/react-components/components/RadioGroup/RecurrenceTypeRadioGroup.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import {radioButton} from "./RadioGroup.module.scss";
+import classNames from "classnames";
+import PropTypes from "prop-types";
+import Label from '../Label/Label.jsx';
+import InputNumber from "../InputNumber/InputNumber.jsx";
+
+
+const RecurrenceTypeRadioGroup = props => {
+    const {onChange, onFrequencyChange, frequency} = props;
+    const groupName = "recurrenceType";
+    return (<div>
+        <InputNumber onInputChange={onFrequencyChange} defaultValue={frequency}/>
+        <input
+            type="radio"
+            value="Day"
+            name={groupName}
+            onChange={onChange}
+        />
+        <Label translationKey="DAY_LABEL" defaultValue="Day"/>
+        <input
+            type="radio"
+            value="week"
+            name={groupName}
+            onChange={onChange}
+        />
+        <Label translationKey="WEEK_LABEL" defaultValue="Week"/>
+    </div>)
+};
+
+RecurrenceTypeRadioGroup.propTypes = {
+    intl: PropTypes.object.isRequired,
+    frequency: PropTypes.number,
+    onChange: PropTypes.func,
+    onFrequencyChange: PropTypes.func
+};
+
+export default RecurrenceTypeRadioGroup;
