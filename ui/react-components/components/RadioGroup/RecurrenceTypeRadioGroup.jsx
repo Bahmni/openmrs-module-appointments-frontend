5d3b7f8547242ce2e4da37db428abff872e196fd
Alekhya <alekhyayalla23@gmail.com>
2019-10-21 18:42:12 +0530

    Bindu, Alekhya | MOBN-620 | Add css for recurrence type radio group

--- ui/react-components/components/RadioGroup/RecurrenceTypeRadioGroup.jsx
+++ ui/react-components/components/RadioGroup/RecurrenceTypeRadioGroup.jsx
@@ -3,26 +3,43 @@ import PropTypes from "prop-types";
 import Label from '../Label/Label.jsx';
 import InputNumber from "../InputNumber/InputNumber.jsx";
 import {injectIntl} from "react-intl";
+import classNames from 'classnames';
+import {
+    frequencyInput,
+    inputNumber,
+    radioButton,
+    radioGroup,
+    recurrenceType
+} from "./RecurrenceTypeRadioGroup.module.scss"
 
 const RecurrenceTypeRadioGroup = props => {
     const {onChange, onFrequencyChange, frequency} = props;
     const groupName = "recurrenceType";
-    return (<div>
-        <InputNumber onInputChange={onFrequencyChange} defaultValue={frequency}/>
-        <input
-            type="radio"
-            value="Day"
-            name={groupName}
-            onChange={onChange}
-        />
-        <Label translationKey="DAY_LABEL" defaultValue="Day"/>
-        <input
-            type="radio"
-            value="week"
-            name={groupName}
-            onChange={onChange}
-        />
-        <Label translationKey="WEEK_LABEL" defaultValue="Week"/>
+    return (<div className={classNames(recurrenceType)}>
+        <div className={classNames(inputNumber)}>
+            <InputNumber onInputChange={onFrequencyChange} defaultValue={frequency}/>
+        </div>
+        <div className={classNames(radioGroup)}>
+            <div className={classNames(radioButton)}>
+                <input
+                    type="radio"
+                    value="Day"
+                    name={groupName}
+                    onChange={onChange}
+                />
+                <Label translationKey="DAY_LABEL" defaultValue="Day"/>
+            </div>
+            <div>
+                <input
+                    type="radio"
+                    value="week"
+                    name={groupName}
+                    onChange={onChange}
+                />
+                <Label translationKey="WEEK_LABEL" defaultValue="Week"/>
+            </div>
+        </div>
+
     </div>)
 };
 
