f71cfd968e24c988c85b4c9fa7cb21a2c8081981
Sneha Bagri <bsneha@thoughtworks.com>
2019-10-22 12:48:27 +0530

    Sneha | MOBN-613 | Make sure the current date is not selected by default on calendar

--- ui/react-components/components/DatePicker/DatePicker.jsx
+++ ui/react-components/components/DatePicker/DatePicker.jsx
@@ -1,31 +1,37 @@
-import React, { Text } from 'react';
+import React, {Text, useState} from 'react';
 import Calendar from 'rc-calendar';
 import 'rc-calendar/assets/index.css';
-import { appointmentDatePicker } from './DatePicker.module.scss';
+import { appointmentDatePicker, appointmentDatePickerSelected, appointmentDatePickerNotSelected } from './DatePicker.module.scss';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import moment from 'moment';
 
 const AppointmentDatePicker = (props) => {
-
+    const [value, setValue] = useState(null);
     const disablePastDates = (current) => {
         if (!current) {
             return false;
         }
-        const date = moment().subtract('1','days').endOf('day');
+        const date = moment().subtract('1', 'days').endOf('day');
         return current.isBefore(date);
     };
+    const onChange = (date) => { setValue(date); props.onChange(date); };
+    const onClear = () => { setValue(null); props.onClear(); };
+    let styles = [appointmentDatePicker];
+    value ? styles.push(appointmentDatePickerSelected)
+        : styles.push(appointmentDatePickerNotSelected);
 
     return (
         <div data-testid="datePicker">
             <Calendar
-                showToday={false}
                 showOk={false}
-                onChange={props.onChange}
+                showToday={false}
+                onClear={onClear}
                 disabledDate={disablePastDates}
-                className={classNames(appointmentDatePicker)}
-                onClear={props.onClear}
+                selectedValue={value}
+                className={classNames(styles)}
                 dateInputPlaceholder="mm/dd/yyyy"
+                onSelect={onChange}
             />
         </div>
     );
