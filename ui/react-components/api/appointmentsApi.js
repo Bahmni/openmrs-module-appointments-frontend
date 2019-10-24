4742434a1361eda39429daa1a06abc0c16abc6e9
Alekhya <alekhyayalla23@gmail.com>
2019-10-24 13:50:19 +0530

    Bindu, Alekhya | MOBN-620 | Save recurring appointments on click of check & save

--- ui/react-components/api/appointmentsApi.js
+++ ui/react-components/api/appointmentsApi.js
@@ -1,10 +1,18 @@
 import axios from 'axios';
-import {appointmentsSaveUrl} from "../constants";
+import {appointmentsSaveUrl, recurringAppointmentsSaveUrl} from "../constants";
 
 export const saveOrUpdateAppointment = async (data) => {
     try {
         const response = await axios.post(`${appointmentsSaveUrl}`, data);
         return response;
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+export const saveRecurringAppointments = async data => {
+    try {
+        return await axios.post(`${recurringAppointmentsSaveUrl}`, data);
     } catch (error) {
         console.error(error);
     }
