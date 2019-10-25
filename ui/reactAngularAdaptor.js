71213cdde2093809eab1b5e61814b6ff0acf6e10
venkatavineela <vineela.vini421.vv@gmail.com>
2019-10-25 15:21:06 +0530

    Vineela, Sowmika | MOBN-737 | Change viewDate to saved appointment date on close of popup and implement feedback from comments

--- ui/reactAngularAdaptor.js
+++ ui/reactAngularAdaptor.js
@@ -6,13 +6,15 @@ angular.module('bahmni.appointments')
 
 // Used this component to pass a callback function to react component
 angular.module('bahmni.appointments').component('reactAddAppointmentWrapper',{
-    template: '<react-add-appointment on-back="onBack">',
+    template: '<react-add-appointment on-back="onBack" state="state">',
     controller: reactAddAppointmentController
 });
 
 reactAddAppointmentController.$inject = ['$scope', '$state'];
 function reactAddAppointmentController($scope, $state) {
-    $scope.onBack = function () {
-        $state.go('^', $state.params, {reload: true});
-    }
+    $scope.onBack = function (state) {
+        $state.go('^', state.params, {reload: true});
+    };
+
+    $scope.state = $state;
 }
