11e83a29164c0ed8007cf67e74bb4456e16d2cbc
Alekhya <alekhyayalla23@gmail.com>
2019-10-25 17:07:05 +0530

    Bindu, Alekhya | MOBN-620 | CSS fixes

--- ui/react-components/components/Label/Label.jsx
+++ ui/react-components/components/Label/Label.jsx
@@ -3,15 +3,16 @@ import {FormattedMessage} from 'react-intl';
 import PropTypes from 'prop-types';
 
 const Label = props => {
-    const { translationKey, defaultValue } = props;
+    const {translationKey, defaultValue, forInput} = props;
     return (
-        <label><b><FormattedMessage id={translationKey} defaultMessage={defaultValue}/></b></label>
+        <label for={forInput}><b><FormattedMessage id={translationKey} defaultMessage={defaultValue}/></b></label>
     )
 }
 
 Label.propTypes = {
     translationKey: PropTypes.string.isRequired,
-    defaultValue: PropTypes.string.isRequired
+    defaultValue: PropTypes.string.isRequired,
+    forInput: PropTypes.string
 };
 
 export default Label;
