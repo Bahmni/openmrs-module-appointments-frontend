4b8a10e8807fe25a208c2d2fb395b8f72e275f04
Alekhya <alekhyayalla23@gmail.com>
2019-10-28 20:57:55 +0530

    Bindu, Alekhya | MOBN-620 | Extract recurring appointments api into separate module

---
+++ ui/react-components/api/recurringAppointmentsApi.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+import {recurringAppointmentsSaveUrl} from "../constants";
+
+export const saveRecurringAppointments = async data => {
+    try {
+        return await axios.post(`${recurringAppointmentsSaveUrl}`, data);
+    } catch (error) {
+        console.error(error);
+    }
+};
