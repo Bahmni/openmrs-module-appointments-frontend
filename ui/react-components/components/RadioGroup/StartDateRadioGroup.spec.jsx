a3ff6ab9d6587f5ded4a1107d00f9b810cb3f77d
Alekhya <alekhyayalla23@gmail.com>
2019-10-28 15:29:54 +0530

    Bindu, Alekhya | MOBN-620 | Fix warnings

--- ui/react-components/components/RadioGroup/StartDateRadioGroup.spec.jsx
+++ ui/react-components/components/RadioGroup/StartDateRadioGroup.spec.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
-import {render, fireEvent } from '@testing-library/react';
+import {fireEvent} from '@testing-library/react';
 import {renderWithReactIntl} from '../../utils/TestUtil';
 import StartDateRadioGroup from "./StartDateRadioGroup.jsx";
-import {FROM, TODAY} from "../../constants";
 
 
 describe('EndDateRadioGroup', () => {
     it('should render StartDateRadioGroup component', () => {
-        const {container, getByText} = renderWithReactIntl(<StartDateRadioGroup/>);
+        const {container, getByText} = renderWithReactIntl(<StartDateRadioGroup onChange={jest.fn()}/>);
         expect(getByText('Today')).not.toBeNull();
         expect(getByText('From')).not.toBeNull();
         expect(container.querySelector('.radioButton')).not.toBeNull();
@@ -16,7 +15,7 @@ describe('EndDateRadioGroup', () => {
     });
 
     it('should call onChange on click of `Today` radio button', () => {
-        const {container, queryAllByDisplayValue } = renderWithReactIntl(<StartDateRadioGroup/>);
+        const {container, queryAllByDisplayValue } = renderWithReactIntl(<StartDateRadioGroup onChange={jest.fn()}/>);
         const radio = queryAllByDisplayValue(/TODAY/);
         expect(radio[0].checked).toBe(false);
         fireEvent.change(radio[0], {target: {checked: true}});
@@ -24,7 +23,7 @@ describe('EndDateRadioGroup', () => {
     });
 
     it('should call onChange on click of `From` radio button', () => {
-        const {container, queryAllByDisplayValue} = renderWithReactIntl(<StartDateRadioGroup/>);
+        const {container, queryAllByDisplayValue} = renderWithReactIntl(<StartDateRadioGroup onChange={jest.fn()}/>);
         const radio = queryAllByDisplayValue(/FROM/);
         expect(radio[0].checked).toBe(false);
         fireEvent.change(radio[0], {target: {checked: true}});
@@ -32,7 +31,7 @@ describe('EndDateRadioGroup', () => {
     });
 
     it('`From` label should be grayed out when `Today` radio button selected', () => {
-        const {container, queryAllByDisplayValue} = renderWithReactIntl(<StartDateRadioGroup startDateType="TODAY"/>);
+        const {container, queryAllByDisplayValue} = renderWithReactIntl(<StartDateRadioGroup startDateType="TODAY" onChange={jest.fn()}/>);
         const radioToday = queryAllByDisplayValue(/TODAY/);
         const radioFrom = queryAllByDisplayValue(/FROM/);
         expect(radioToday[0].checked).toBe(true);
