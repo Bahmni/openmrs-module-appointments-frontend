c6aa210d06c4dd24abace42c91e873f4a061b71e
Alekhya <alekhyayalla23@gmail.com>
2019-10-29 14:43:44 +0530

    Alekhya | MOBN-620 | Fix occurrences spell, extract helper methods, feedback fixes

---
+++ ui/react-components/helper.js
@@ -0,0 +1,27 @@
+import {minDurationForAppointment} from "./constants";
+import moment from "moment";
+
+export const isSpecialitiesEnabled = appConfig => {
+    if (appConfig)
+        return appConfig.enableSpecialities;
+    return false;
+};
+
+export const getDefaultOccurrences = appConfig => {
+    if (appConfig && appConfig.recurrence)
+        return Number(appConfig.recurrence.defaultNumberOfOccurrences);
+};
+
+export const maxAppointmentProvidersAllowed = appConfig => {
+    if (appConfig && appConfig.maxAppointmentProviders)
+        return appConfig.maxAppointmentProviders;
+    return 1;
+};
+
+export const getDuration = (service, serviceType) => (serviceType && serviceType.duration)
+    || (service && service.durationMins)
+    || minDurationForAppointment;
+
+export const getYesterday = () => {
+    return moment().subtract('1', 'days').endOf('day');
+};
