080f66c4885c31511d66be0d5a773023b1721dd8
tbindu <tbindu@thoughtworks.com>
2019-10-29 17:49:05 +0530

    Alekhya, Bindu | MOBN-620 | fixed css styling for checkbox

--- ui/react-components/components/RecurringPlan/RecurringPlan.jsx
+++ ui/react-components/components/RecurringPlan/RecurringPlan.jsx
@@ -4,12 +4,14 @@ import {checkbox, checkboxContainer, planLabel} from "./RecurringPlan.module.scs
 import classNames from "classnames";
 import PropTypes from "prop-types";
 import {injectIntl} from "react-intl";
+import Checkbox from 'rc-checkbox';
+import 'rc-checkbox/assets/index.css';
 
 const RecurringPlan = props => {
     return (<div>
         <span className={classNames(planLabel)}><Label className={classNames(planLabel)} translationKey="PLAN_LABEL" defaultValue="Plan"/></span>
-        <div className={classNames(checkboxContainer)}><input
-            type="checkbox"
+        <div className={classNames(checkboxContainer)} data-test-id="checkbox">
+            <Checkbox
             onChange={props.onChange}
             defaultChecked={false}
             className={classNames(checkbox)}
