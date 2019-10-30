1b0a131dce3a2c40ee50cebbef9bc1a3c818f975
sowmika <sowmika148@gmail.com>
2019-10-30 15:52:02 +0530

    Sowmika, Vinay | MOBN-754 | Add button selected css

--- ui/react-components/components/ButtonGroup/ButtonGroup.spec.jsx
+++ ui/react-components/components/ButtonGroup/ButtonGroup.spec.jsx
@@ -1,26 +1,71 @@
 import {renderWithReactIntl} from "../../utils/TestUtil";
 import ButtonGroup from "./ButtonGroup.jsx";
 import React from "react";
+import {fireEvent} from "@testing-library/react";
 
 describe('Button Group', () => {
     it('should display the given content in buttons', () => {
-        const buttonsList = [
-            {
+        const buttonsList = {
+            ONE: {
                 translationKey: 'ONE',
                 defaultValue: 'One'
             },
-            {
+            TWO: {
                 translationKey: 'TWO',
                 defaultValue: 'Two'
             },
-            {
+            THREE: {
                 translationKey: 'THREE',
                 defaultValue: 'Three'
             }
-        ];
-        const {getByText} = renderWithReactIntl(<ButtonGroup buttonsList={buttonsList}/>);
+        };
+        const {getByText} = renderWithReactIntl(<ButtonGroup buttonsList={buttonsList} onClick={jest.fn()}/>);
         getByText('One');
         getByText('Two');
         getByText('Three');
+    });
+
+    it('should get selected className for selected buttons', () => {
+        const buttonsList = {
+            ONE: {
+                translationKey: 'ONE',
+                defaultValue: 'One',
+                isSelected: true
+            },
+            TWO: {
+                translationKey: 'TWO',
+                defaultValue: 'Two',
+                isSelected: true
+            },
+            THREE: {
+                translationKey: 'THREE',
+                defaultValue: 'Three',
+                isSelected: false
+            }
+        };
+
+        const {container} = renderWithReactIntl(<ButtonGroup buttonsList={buttonsList} onClick={jest.fn()}/>);
+        expect(container.querySelectorAll('.selected').length).toBe(2);
+    });
+
+    it('should call onClick when button is clicked', () => {
+        const buttonsList = {
+            ONE: {
+                translationKey: 'ONE',
+                defaultValue: 'One',
+                isSelected: true
+            },
+            TWO: {
+                translationKey: 'TWO',
+                defaultValue: 'Two',
+                isSelected: true
+            }
+        };
+
+        let onClickSpy = jest.fn();
+        const {getByText} = renderWithReactIntl(<ButtonGroup buttonsList={buttonsList} onClick={onClickSpy}/>);
+        fireEvent.click(getByText('One'));
+        fireEvent.click(getByText('Two'));
+        expect(onClickSpy).toHaveBeenCalledTimes(2);
     })
 });
