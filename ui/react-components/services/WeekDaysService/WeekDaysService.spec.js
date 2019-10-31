520a367f6f841ea5e69342fbf21920da3f728ff7
sowmika <sowmika148@gmail.com>
2019-10-31 14:45:07 +0530

    Sowmika, Vinay | MOBN-754 | Add week days in save payload and error handling

--- ui/react-components/services/WeekDaysService/WeekDaysService.spec.js
+++ ui/react-components/services/WeekDaysService/WeekDaysService.spec.js
@@ -1,10 +1,47 @@
 import React from "react";
-import {getWeekDays} from "./WeekDaysService";
+import {getSelectedWeekDays, getWeekDays} from "./WeekDaysService";
 
 describe('Weekdays Service', () => {
     it('should reorder the weekdays based on start of week', () => {
         const startOfWeek = 'Wednesday';
         const weekDays = getWeekDays(startOfWeek);
         expect([...weekDays.keys()]).toStrictEqual(['WEDNESDAY', 'THURSDAY', 'FRIDAY', 'SATURDAY', 'SUNDAY', 'MONDAY', 'TUESDAY']);
+    });
+
+    it('should get selected week days', () => {
+        const weekDays = new Map([
+            ['SUNDAY', {
+                translationKey: 'SUNDAY',
+                defaultValue: 'Su',
+                isSelected: true
+            }], ['MONDAY', {
+                translationKey: 'MONDAY',
+                defaultValue: 'Mo',
+                isSelected: false
+            }], ['TUESDAY', {
+                translationKey: 'TUESDAY',
+                defaultValue: 'Tu',
+                isSelected: false
+            }], ['WEDNESDAY', {
+                translationKey: 'WEDNESDAY',
+                defaultValue: 'We',
+                isSelected: true
+            }], ['THURSDAY', {
+                translationKey: 'THURSDAY',
+                defaultValue: 'Th',
+                isSelected: false
+            }], ['FRIDAY', {
+                translationKey: 'FRIDAY',
+                defaultValue: 'Fr',
+                isSelected: false
+            }], ['SATURDAY', {
+                translationKey: 'SATURDAY',
+                defaultValue: 'Sa',
+                isSelected: true
+            }]
+        ]);
+        const selectedWeekDays = getSelectedWeekDays(weekDays);
+        expect(selectedWeekDays.length).toBe(3);
+        expect(selectedWeekDays.includes("SATURDAY", "WEDNESDAY", "SUNDAY")).toBeTruthy();
     });
 });
