e32e43b907d69bb78eeea728f3c82e672a708a92
sowmika <sowmika148@gmail.com>
2019-11-04 14:19:04 +0530

    Sowmika, Vinay | MOBN-623 | Add getappointmentByUuid api service to get appointment

--- ui/react-components/api/recurringAppointmentsApi.js
+++ ui/react-components/api/recurringAppointmentsApi.js
@@ -1,9 +1,18 @@
 import axios from "axios";
-import {recurringAppointmentsConflictsUrl, recurringAppointmentsSaveUrl} from "../constants";
+import {recurringAppointmentByUuidUrl, recurringAppointmentsConflictsUrl, recurringAppointmentsSaveUrl} from "../constants";
 
 export const saveRecurringAppointments = async data => {
     try {
         return await axios.post(`${recurringAppointmentsSaveUrl}`, data);
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+export const getRecurringAppointmentByUuid = async (appointmentUuid) => {
+    try {
+        const response = await axios.get(`${recurringAppointmentByUuidUrl}?uuid=${appointmentUuid}`);
+        return response;
     } catch (error) {
         console.error(error);
     }
