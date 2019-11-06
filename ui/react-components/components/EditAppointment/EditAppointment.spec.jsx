db71c73a66bebb6412a56533e4011802c88ffd9b
sowmika <sowmika148@gmail.com>
2019-11-06 12:00:16 +0530

    Sowmika, Vinay | MOBN-623 | Add time and date components in edit appointment screen

--- ui/react-components/components/EditAppointment/EditAppointment.spec.jsx
+++ ui/react-components/components/EditAppointment/EditAppointment.spec.jsx
@@ -36,15 +36,17 @@ describe('Edit Appointment', () => {
     });
 
     //TODO Warnings while running tests
-    it('should render patient, service, service type, speciality and location details coming from response', async () => {
+    it('should render appointment details coming from response', async () => {
         let getByTextInDom = undefined;
+        let containerInDom = undefined;
         const config = {
             "enableSpecialities": true
         };
         act(() => {
-            const {getByText} = renderWithReactIntl(<EditAppointment
+            const {getByText, container} = renderWithReactIntl(<EditAppointment
                 appointmentUuid={'36fdc60e-7ae5-4708-9fcc-8c98daba0ca9'} isRecurring={false} appConfig={config}/>);
             getByTextInDom = getByText;
+            containerInDom = container;
         });
         await flushPromises();
         getByTextInDom('9DEC81BF 9DEC81C6 (IQ1114)');
@@ -52,12 +54,17 @@ describe('Edit Appointment', () => {
         getByTextInDom('1 session');
         getByTextInDom('Operating Theatre');
         getByTextInDom('test speciality');
+        getByTextInDom('to');
+        getByTextInDom('10:00 am');
+        getByTextInDom('10:30 am');
+        expect(containerInDom.querySelectorAll('.rc-time-picker-input')[0].value).toBe('10:00 am');
+        expect(containerInDom.querySelectorAll('.rc-time-picker-input')[1].value).toBe('10:30 am');
     });
 
     it('should recurring plan component', () => {
         const {getByText} = renderWithReactIntl(<EditAppointment appointmentUuid={'appt-uuid'} isRecurring={true}/>);
         getByText('Plan');
         getByText('Recurring Appointment');
-    })
+    });
 });
 
