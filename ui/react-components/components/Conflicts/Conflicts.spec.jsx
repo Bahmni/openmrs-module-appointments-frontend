3b4cc2059855a3056af58521499ae8dfdffab43e
Alekhya <alekhyayalla23@gmail.com>
2019-11-06 12:51:39 +0530

    Siva, Alekhya | MOBN-619 | Add patient double booking conflicts tab panel content

--- ui/react-components/components/Conflicts/Conflicts.spec.jsx
+++ ui/react-components/components/Conflicts/Conflicts.spec.jsx
@@ -4,7 +4,7 @@ import {renderWithReactIntl} from "../../utils/TestUtil";
 
 describe('Conflicts', () => {
     it('should render conflicts footer component', () => {
-        const {container} = renderWithReactIntl(<Conflicts conflicts={{}}/>, {});
+        const {container} = renderWithReactIntl(<Conflicts conflicts={{}} service={{}}/>, {});
         expect(container.querySelector('.conflicts')).not.toBeNull();
         expect(container.querySelector('.conflicts .footer')).not.toBeNull();
         expect(container.querySelector('.conflicts .body')).not.toBeNull();
