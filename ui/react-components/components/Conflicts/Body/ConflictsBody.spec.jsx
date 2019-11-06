ac0f4fbca2bb69c57eb0831e0b96ead43baaa13c
Alekhya <alekhyayalla23@gmail.com>
2019-11-06 12:51:39 +0530

    Siva, Alekhya | MOBN-619 | Add patient double booking conflicts tab panel content

--- ui/react-components/components/Conflicts/Body/ConflictsBody.spec.jsx
+++ ui/react-components/components/Conflicts/Body/ConflictsBody.spec.jsx
@@ -5,21 +5,28 @@ import {renderWithReactIntl} from "../../../utils/TestUtil";
 
 describe('Conflicts Body', () => {
 
-    const messages = {'OVERLAPPING_CONFLICTS': 'Patient double booking',
-        'NO_SERVICE_DATE_CONFLICTS':'Service unavailability'};
-    it('should render patient double booking and service unavailable tabs', function () {
-        const conflicts = {SERVICE_UNAVAILABLE: [{uuid: "uuid"}], PATIENT_DOUBLE_BOOKING: [{uuid: "uuid"}]};
+    const messages = {
+        'OVERLAPPING_CONFLICTS': 'Patient double booking',
+        'NO_SERVICE_DATE_CONFLICTS': 'Service unavailability'
+    };
+    const service = {name: 'Orthopedic'};
 
-        const {getByText} = renderWithReactIntl(<ConflictsBody conflicts={conflicts}/>, messages);
+    it('should render patient double booking and service unavailable tabs', function () {
+        const conflicts = {
+            SERVICE_UNAVAILABLE: [{service: {name: "Dressing"}, uuid: "uuid"}],
+            PATIENT_DOUBLE_BOOKING: [{service: {name: "Dressing"}, uuid: "uuid"}]
+        };
+
+        const {getByText} = renderWithReactIntl(<ConflictsBody conflicts={conflicts} service={service}/>, messages);
 
         expect(getByText("Patient double booking")).not.toBeNull();
         expect(getByText("Service unavailability")).not.toBeNull();
     });
 
     it('should render patient double booking tab', function () {
-        const conflicts = {PATIENT_DOUBLE_BOOKING: [{uuid: "uuid"}]};
+        const conflicts = {PATIENT_DOUBLE_BOOKING: [{service: {name: "Dressing"}, uuid: "uuid"}]};
 
-        const {getByText} = renderWithReactIntl(<ConflictsBody conflicts={conflicts}/>, messages);
+        const {getByText} = renderWithReactIntl(<ConflictsBody conflicts={conflicts} service={service}/>, messages);
 
         expect(getByText("Patient double booking")).not.toBeNull();
     });
@@ -27,10 +34,20 @@ describe('Conflicts Body', () => {
     it('should not render any tabs if conflicts don\'t exist', function () {
         const conflicts = {};
 
-        const {queryByText} = renderWithReactIntl(<ConflictsBody conflicts={conflicts}/>, messages);
+        const {queryByText} = renderWithReactIntl(<ConflictsBody conflicts={conflicts} service={service}/>, messages);
 
         expect(queryByText('Overlapping conflicts')).toBeNull();
         expect(queryByText('No-Service Date conflicts')).toBeNull();
     });
+
+    it('should render patient double booking conflicts content', () => {
+        const conflicts = {PATIENT_DOUBLE_BOOKING: [{service: {name: "Dressing"}, startDateTime: 1575561600000}]};
+        const {container, getByText} = renderWithReactIntl(<ConflictsBody conflicts={conflicts} service={service}/>);
+
+        getByText('The recurring appointments you are trying to book overlaps with the following dates');
+        getByText('5th December ‘19 | Thursday | 9:30 PM');
+        expect(container.querySelector('.conflictMessage').innerHTML).toContain('Current Orthopedic request');
+        expect(container.querySelector('.boldContent').innerHTML).toContain('conflicts with Dressing');
+    })
 
 });
