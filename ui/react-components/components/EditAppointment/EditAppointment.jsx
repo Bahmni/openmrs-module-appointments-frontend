92cce7f2579f1ee2b0abd8693eac2a76ad5f4636
sowmika <sowmika148@gmail.com>
2019-11-06 12:00:16 +0530

    Sowmika, Vinay | MOBN-623 | Add time and date components in edit appointment screen

--- ui/react-components/components/EditAppointment/EditAppointment.jsx
+++ ui/react-components/components/EditAppointment/EditAppointment.jsx
@@ -4,7 +4,7 @@ import React from "react";
 import {injectIntl} from "react-intl";
 import classNames from "classnames";
 import {
-    appointmentEditor,
+    appointmentEditor, recurringContainer, recurringContainerLeft,
     searchFieldsContainer,
     searchFieldsContainerLeft
 } from "../AddAppointment/AddAppointment.module.scss";
@@ -13,9 +13,13 @@ import {getRecurringAppointmentByUuid} from "../../api/recurringAppointmentsApi"
 import {getAppointmentByUuid} from "../../api/appointmentsApi";
 import {getPatientForDropdown} from "../../mapper/patientMapper";
 import moment from "moment";
-import {getDuration} from "../../helper";
+import {getDuration, getYesterday} from "../../helper";
 import {MINUTES} from "../../constants";
 import RecurringPlan from "../RecurringPlan/RecurringPlan.jsx";
+import Label from "../Label/Label.jsx";
+import AppointmentDatePicker from "../DatePicker/DatePicker.jsx";
+import {editAppointment, currentTimeSlot} from './EditAppointment.module.scss'
+import TimeSelector from "../TimeSelector/TimeSelector.jsx";
 
 const EditAppointment = props => {
 
@@ -78,6 +82,9 @@ const EditAppointment = props => {
         }
     };
 
+    const [currentStartTime, setCurrentStartTime] = useState();
+    const [currentEndTime, setCurrentEndTime] = useState();
+
     const generateAppointmentDetails = async () => {
         const appointment = isRecurring ? await getRecurringAppointmentByUuid(appointmentUuid) : await getAppointmentByUuid(appointmentUuid);
         const appointmentResponse = isRecurring
@@ -90,12 +97,14 @@ const EditAppointment = props => {
                 service: {label: appointmentResponse.service.name, value: appointmentResponse.service},
                 serviceType: appointmentResponse.serviceType ? {label: appointmentResponse.serviceType.name, value: appointmentResponse.serviceType} : undefined,
                 location: appointmentResponse.location ? {label: appointmentResponse.location.name, value: appointmentResponse.location} : undefined,
-                speciality: appointmentResponse.service.speciality.uuid ? {label: appointmentResponse.service.speciality.name, value: appointmentResponse.service.speciality} : undefined,
-                startTime: appointmentResponse.startTime,
-                endTime: appointmentResponse.endTime,
+                speciality: appointmentResponse.service.speciality !== {} ? {label: appointmentResponse.service.speciality.name, value: appointmentResponse.service.speciality} : undefined,
+                startTime: moment(new Date(appointmentResponse.startDateTime)),
+                endTime: moment(new Date(appointmentResponse.endDateTime)),
                 notes: appointmentResponse.notes,
                 isRecurring: isRecurring
             });
+            setCurrentStartTime(moment(new Date(appointmentResponse.startDateTime)).format('hh:mm a'));
+            setCurrentEndTime(moment(new Date(appointmentResponse.endDateTime)).format('hh:mm a'));
             if (isRecurring) {
                 updateAppointmentDetails({
                     recurringStartDate: appointmentResponse.recurringStartDate,
@@ -109,10 +118,22 @@ const EditAppointment = props => {
                 });
             } else {
                 updateAppointmentDetails({
-                    appointmentDate: appointmentResponse.appointmentDate,
+                    appointmentDate: moment(new Date(appointmentResponse.startDateTime)),
                 });
             }
         }
+    };
+
+    const appointmentStartTimeProps = {
+        translationKey: 'APPOINTMENT_TIME_FROM_LABEL', defaultValue: 'From',
+        placeHolderTranslationKey: 'CHOOSE_TIME_PLACE_HOLDER', placeHolderDefaultMessage: 'Enter time as hh:mm am/pm',
+        defaultTime: appointmentDetails.startTime
+    };
+
+    const appointmentEndTimeProps = {
+        translationKey: 'APPOINTMENT_TIME_TO_LABEL', defaultValue: 'To',
+        placeHolderTranslationKey: 'CHOOSE_TIME_PLACE_HOLDER', placeHolderDefaultMessage: 'Enter time as hh:mm am/pm',
+        defaultTime: appointmentDetails.endTime
     };
 
     useEffect(() => {
@@ -120,7 +141,7 @@ const EditAppointment = props => {
     }, [appConfig]);
 
     return (<Fragment>
-        <div data-testid="appointment-editor" className={classNames(appointmentEditor)}>
+        <div data-testid="appointment-editor" className={classNames(appointmentEditor, editAppointment)}>
             <SearchFieldsContainer appointmentDetails={appointmentDetails} errors={errors}
                                    updateErrorIndicators={updateErrorIndicators}
                                    endTimeBasedOnService={endTimeBasedOnService}
@@ -128,6 +149,43 @@ const EditAppointment = props => {
             <div className={classNames(searchFieldsContainer)} data-testid="recurring-plan-checkbox">
                 <div className={classNames(searchFieldsContainerLeft)}>
                     <RecurringPlan isRecurring={appointmentDetails.isRecurring}/>
+                </div>
+            </div>
+            <div className={classNames(recurringContainer)}>
+                <div className={classNames(recurringContainerLeft)}>
+                    <div data-testid="date-selector">
+                        <Label translationKey='CHANGE_DATE_TO_LABEL' defaultValue='Change date to'/>
+                        <AppointmentDatePicker
+                            onChange={date => {
+                                updateAppointmentDetails({appointmentDate: date});
+                                updateErrorIndicators({appointmentDateError: !date});
+                            }}
+                            onClear={() => updateAppointmentDetails({appointmentDate: undefined})}
+                            defaultValue={appointmentDetails.appointmentDate}
+                            minDate={getYesterday()}/>
+                    </div>
+                    <div>
+                        <Label translationKey="CURRENT_TIME_SLOT_LABEL" defaultValue="Current time slot"/>
+                        <div className={classNames(currentTimeSlot)}>
+                            <span>{currentStartTime}</span>
+                            <span> to </span>
+                            <span>{currentEndTime}</span>
+                        </div>
+                        <Label translationKey="APPOINTMENT_TIME_LABEL" defaultValue="Choose a time slot"/>
+                        <div data-testid="start-time-selector">
+                            <TimeSelector {...appointmentStartTimeProps}
+                                          onChange={time => {
+                                              updateAppointmentDetails({startTime: time});
+                                              endTimeBasedOnService(time, appointmentDetails.service, appointmentDetails.serviceType);
+                                          }}/>
+                        </div>
+                        <div data-testid="end-time-selector">
+                            <TimeSelector {...appointmentEndTimeProps}
+                                          onChange={time => {
+                                              updateAppointmentDetails({endTime: time});
+                                          }}/>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
