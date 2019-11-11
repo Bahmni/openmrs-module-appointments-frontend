2538d594fa42e91e4612c46bbfebf1f05a4b33a8
Alekhya <alekhyayalla23@gmail.com>
2019-11-11 13:15:24 +0530

    Siva, Alekhya, Neha | MOBN-619 | Feedback: Extract conflicts as presentational components and few renames

---
+++ ui/react-components/components/Conflicts/ServiceUnavailableConflicts.jsx
@@ -0,0 +1,38 @@
+import classNames from "classnames";
+import {appointmentConflict, conflictDetails, conflictsHeading, conflictsList} from "./Body/ConflictsBody.module.scss";
+import {FormattedMessage} from "react-intl";
+import React from "react";
+import PropTypes from "prop-types";
+import {getAppointmentConflictDetails} from "./ConflictsUtil";
+
+const ServiceUnavailableConflicts = props => {
+
+    const getConflictsList = () => {
+        let conflictList = [];
+        props.conflicts.forEach((conflict, index) => {
+            conflictList.push(<div className={classNames(appointmentConflict)} key={index}>
+                <div className={classNames(conflictDetails)}>{getAppointmentConflictDetails(conflict)}</div>
+            </div>);
+        });
+        return conflictList;
+    };
+    const defaultMessage = `The ${props.service.name} service you had selected for the appointment(s) is not 
+                                available during below listed dates`;
+    return (
+        <div>
+            <div className={classNames(conflictsHeading)}>
+                <FormattedMessage id="NO_SERVICE_CONFLICTS_DEFAULT_TEXT" defaultMessage={defaultMessage}/>
+            </div>
+            <div className={classNames(conflictsList)}>
+                <ul>{getConflictsList()}</ul>
+            </div>
+        </div>
+    );
+};
+
+ServiceUnavailableConflicts.propTypes = {
+    conflicts: PropTypes.array.isRequired,
+    service: PropTypes.object.isRequired
+};
+
+export default ServiceUnavailableConflicts;
