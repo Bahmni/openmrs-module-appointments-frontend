72fd317575195d82bef5047ad0e899caa90b314b
Siva Rachakonda <shiva.rachakonda139@gmail.com>
2019-11-12 13:02:33 +0530

    iva | MOBN-619 | Add cancel button for conflicts slider and remove unused css

--- ui/react-components/components/Conflicts/Conflicts.jsx
+++ ui/react-components/components/Conflicts/Conflicts.jsx
@@ -3,11 +3,16 @@ import ConflictsFooter from "./Footer/ConflictsFooter.jsx";
 import ConflictsBody from "./Body/ConflictsBody.jsx";
 import PropTypes from "prop-types";
 import {injectIntl} from "react-intl";
+import classNames from 'classnames';
+import {button} from "./Conflicts.module.scss";
 
 const Conflicts = props => {
 
     return (
         <div data-testid="conflicts">
+            <button className={classNames(button)} onClick={props.modifyInformation}>
+                    <i className={classNames("fa", "fa-times")}/>
+                </button>
             <div data-testid="conflicts-body">
                 <ConflictsBody conflicts={props.conflicts} service={props.service}/>
             </div>
