080656a282de29a263d5e73f3f99c96263af6d06
Siva Rachakonda <shiva.rachakonda139@gmail.com>
2019-11-12 15:05:08 +0530

    Siva | MOBN-619 | Add test case for cancel button

--- ui/react-components/components/Conflicts/Conflicts.spec.jsx
+++ ui/react-components/components/Conflicts/Conflicts.spec.jsx
@@ -4,9 +4,10 @@ import {renderWithReactIntl} from "../../utils/TestUtil";
 
 describe('Conflicts', () => {
     it('should render conflicts footer component', () => {
-        const {getByTestId} = renderWithReactIntl(<Conflicts conflicts={{}} service={{}}/>, {});
+        const {getByTestId, container} = renderWithReactIntl(<Conflicts conflicts={{}} service={{}}/>, {});
         expect(getByTestId('conflicts')).not.toBeNull();
         expect(getByTestId('conflicts-footer')).not.toBeNull();
         expect(getByTestId('conflicts-body')).not.toBeNull();
+        expect(container.querySelector('.button')).not.toBeNull();
     });
 });
