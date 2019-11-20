198b0451192791e1962ebb6cae033e290a8a5477
venkatavineela <vineela.vini421.vv@gmail.com>
2019-11-20 14:29:24 +0530

    Vinay, Vineela, Neha | MOBN-625 | Add tests for updating single and recurring appointments

--- ui/react-components/api/recurringAppointmentsApi.spec.js
+++ ui/react-components/api/recurringAppointmentsApi.spec.js
@@ -1,5 +1,5 @@
 import mockAxios from "jest-mock-axios";
-import {getRecurringAppointment, recurringConflictsFor, saveRecurringAppointments} from "./recurringAppointmentsApi";
+import {getRecurringAppointment, recurringConflictsFor, saveRecurringAppointments, updateRecurringAppointments} from "./recurringAppointmentsApi";
 import {recurringAppointmentFetchUrl, appointmentConflictsUrl, recurringAppointmentsConflictsUrl, recurringAppointmentsSaveUrl} from "../constants";
 
 describe('Recurring Appointments Api', () => {
@@ -23,6 +23,31 @@ describe('Recurring Appointments Api', () => {
             "type": "DAY",
             "period": 3
         }
+    };
+
+    let updatePayload = {
+        applyForAll: true,
+        "appointmentRequest": {
+            "patientUuid": "fda50921-d5d5-4493-8de8-6ef54c9d4481",
+            "serviceUuid": "2b87edcf-39ac-4dec-94c9-713b932e847c",
+            "serviceTypeUuid": "50ac6a9c-158a-4743-a6b5-a4f9c9317007",
+            "startDateTime": "2019-10-11T04:30:00.000Z",
+            "endDateTime": "2019-10-11T05:00:00.000Z",
+            "providers": [{
+                "uuid": "dae6561f-cca8-4304-9996-6eae80892d91",
+                "response": "ACCEPTED",
+                "comments": null
+            }],
+            "locationUuid": "8debb630-20e0-11e7-a53f-000c29e530d2",
+            "appointmentKind": "Scheduled",
+            "uuid": "d5e9bc86-8b61-4b53-b112-d633d5c904ed"
+        },
+        "recurringPattern": {
+            "frequency": 2,
+            "type": "DAY",
+            "period": 3
+        },
+        timeZone: "Asia/Calcutta"
     };
 
     it('should save recurring appointments and get saved appointments as response', async () => {
@@ -198,4 +223,101 @@ describe('Recurring Appointments Api', () => {
 
     expect(appointment.data).toEqual(mockResponse);
   });
+
+    it('should update recurring appointments and get saved appointments as response', async () => {
+
+        let mockResponse = [
+            {
+                "uuid": "d5e9bc86-8b61-4b53-b112-d633d5c904ed",
+                "appointmentNumber": "0000",
+                "patient": {
+                    "identifier": "IQ1114",
+                    "name": "9DEC81BF 9DEC81C6",
+                    "uuid": "fda50921-d5d5-4493-8de8-6ef54c9d4481"
+                },
+                "service": {
+                    "appointmentServiceId": 1,
+                    "name": "Physiotherapy OPD",
+                    "description": null,
+                    "speciality": {},
+                    "startTime": "",
+                    "endTime": "",
+                    "maxAppointmentsLimit": null,
+                    "durationMins": null,
+                    "location": {"name": "Physiotherapy", "uuid": "8de35e75-20e0-11e7-a53f-000c29e530d2"},
+                    "uuid": "2b87edcf-39ac-4dec-94c9-713b932e847c",
+                    "color": "#00CED1",
+                    "creatorName": null
+                },
+                "serviceType": {"duration": 30, "name": "1 session", "uuid": "50ac6a9c-158a-4743-a6b5-a4f9c9317007"},
+                "provider": null,
+                "location": {"name": "Operating Theatre", "uuid": "8debb630-20e0-11e7-a53f-000c29e530d2"},
+                "startDateTime": 1570768200000,
+                "endDateTime": 1570770000000,
+                "appointmentKind": "Scheduled",
+                "status": "Scheduled",
+                "comments": null,
+                "additionalInfo": {},
+                "providers": [{
+                    "uuid": "dae6561f-cca8-4304-9996-6eae80892d91",
+                    "comments": null,
+                    "response": "ACCEPTED",
+                    "name": "Abeer Abusamour"
+                }],
+                "recurring": true
+            },
+            {
+                "uuid": "36fdc60e-7ae5-4708-9fcc-8c98daba0ca8",
+                "appointmentNumber": "0000",
+                "patient": {
+                    "identifier": "IQ1114",
+                    "name": "9DEC81BF 9DEC81C6",
+                    "uuid": "fda50921-d5d5-4493-8de8-6ef54c9d4481"
+                },
+                "service": {
+                    "appointmentServiceId": 1,
+                    "name": "Physiotherapy OPD",
+                    "description": null,
+                    "speciality": {},
+                    "startTime": "",
+                    "endTime": "",
+                    "maxAppointmentsLimit": null,
+                    "durationMins": null,
+                    "location": {"name": "Physiotherapy", "uuid": "8de35e75-20e0-11e7-a53f-000c29e530d2"},
+                    "uuid": "2b87edcf-39ac-4dec-94c9-713b932e847c",
+                    "color": "#00CED1",
+                    "creatorName": null
+                },
+                "serviceType": {"duration": 30, "name": "1 session", "uuid": "50ac6a9c-158a-4743-a6b5-a4f9c9317007"},
+                "provider": null,
+                "location": {"name": "Operating Theatre", "uuid": "8debb630-20e0-11e7-a53f-000c29e530d2"},
+                "startDateTime": 1570778200000,
+                "endDateTime": 1570780000000,
+                "appointmentKind": "Scheduled",
+                "status": "Scheduled",
+                "comments": null,
+                "additionalInfo": {},
+                "providers": [{
+                    "uuid": "dae6561f-cca8-4304-9996-6eae80892d91",
+                    "comments": null,
+                    "response": "ACCEPTED",
+                    "name": "Abeer Abusamour"
+                }],
+                "recurring": true
+            }
+        ];
+
+        mockAxios.put.mockImplementationOnce(() =>
+            Promise.resolve({
+                data: mockResponse
+            })
+        );
+
+        let appointment = await updateRecurringAppointments(updatePayload);
+
+        let appointmentUuid = "d5e9bc86-8b61-4b53-b112-d633d5c904ed";
+        expect(mockAxios.put).toHaveBeenCalledWith(`${recurringAppointmentsSaveUrl}/${appointmentUuid}`, updatePayload);
+
+        expect(appointment.data).toEqual(mockResponse);
+    });
 });
