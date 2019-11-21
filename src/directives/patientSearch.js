ddc478a2ce7b341f1c153b7724716efe4d116179
sukreet <sukreet001@gmail.com>
2019-11-21 16:45:35 +0530

    AP-30 | Defaulting patient info on list appointments based on url query param.

--- src/directives/patientSearch.js
+++ src/directives/patientSearch.js
@@ -1,7 +1,8 @@
 'use strict';
 
 angular.module('bahmni.appointments')
-    .directive('patientSearch', ['patientService', 'appointmentsService', 'spinner', '$state', function (patientService, appointmentsService, spinner, $state) {
+    .directive('patientSearch', ['patientService', 'appointmentsService', 'spinner', '$state', '$location',
+        function (patientService, appointmentsService, spinner, $state, $location) {
         return {
             restrict: "E",
             scope: {
@@ -35,6 +36,18 @@ angular.module('bahmni.appointments')
                     if ($state.params.isSearchEnabled && $state.params.patient) {
                         $scope.patient = $scope.responseMap([$state.params.patient])[0].label;
                         $scope.onSelectPatient($state.params.patient);
+                    }
+
+                    if ($location.search()["patient"]) {
+                        const uuid = $location.search()["patient"];
+                        $scope.isSearchEnabled = true;
+                        patientService.getPatient(uuid).then(function (res) {
+                            const patient = res.data;
+                            let displaySplit = patient.display.split(' - ');
+                            $scope.patient =  displaySplit[1] + " (" + displaySplit[0] + ")";
+                            $scope.isFilterOpen = false;
+                            $scope.onSelectPatient(patient);
+                        })
                     }
 
                     $scope.$watch(function () {
