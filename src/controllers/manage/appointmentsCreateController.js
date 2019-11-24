fe404e5dc10efe37b1045a34fae0d61b931bc7fd
MD Dubey <mddubey409@gmail.com>
2019-11-24 16:41:03 +0530

    Dubey | AP-28 | Restrict updates of provider response and status only in case of time changes in edit

--- src/controllers/manage/appointmentsCreateController.js
+++ src/controllers/manage/appointmentsCreateController.js
@@ -17,6 +17,7 @@ angular.module('bahmni.appointments')
             $scope.warning = {};
             $scope.minDuration = Bahmni.Appointments.Constants.minDurationForAppointment;
             const isAppointmentRequestEnabled = appService.getAppDescriptor().getConfigValue('enableAppointmentRequests');
+            let appointmentTimeBeforeEdit = {};
 
             var providerListForCurrentUser = function (providers) {
                 if (appointmentCommonService.isCurrentUserHavingPrivilege(Bahmni.Appointments.Constants.privilegeManageAppointments, $rootScope.currentUser.privileges)) {
@@ -81,6 +82,13 @@ angular.module('bahmni.appointments')
                 if ($scope.appointment.patient) {
                     $scope.onSelectPatient($scope.appointment.patient);
                 }
+                if ($scope.isEditMode()){
+                    appointmentTimeBeforeEdit = {
+                        date: $scope.appointment.date,
+                        startTime: $scope.appointment.startTime,
+                        endTime: $scope.appointment.endTime,
+                    };
+                }
             };
 
             $scope.allowProviderAddition = function () {
@@ -128,7 +136,23 @@ angular.module('bahmni.appointments')
                 }
             };
 
+            const isAtSameTime = function(appointmentTimeBeforeEdit){
+                if (!moment($scope.appointment.date).isSame(moment(appointmentTimeBeforeEdit.date))){
+                    return false;
+                }
+                const newStart = moment($scope.appointment.startTime, 'hh:mm a');
+                const previousStart = moment(appointmentTimeBeforeEdit.startTime, 'hh:mm a');
+                const newEnd = moment($scope.appointment.endTime, 'hh:mm a');
+                const previousEnd = moment(appointmentTimeBeforeEdit.endTime, 'hh:mm a');
+
+                return newStart.isSame(previousStart, 'minutes')
+                    && newEnd.isSame(previousEnd, 'minutes')
+            };
+
             function updateAppointmentStatusAndProviderResponse() {
+                if ($scope.isEditMode() && isAtSameTime(appointmentTimeBeforeEdit)){
+                    return;
+                }
                 const allAppointmentDetails = _.cloneDeep($scope.appointment);
                 const updateStatusAndProviderResponse = Bahmni.Appointments.AppointmentStatusHandler
                     .getUpdatedStatusAndProviderResponse(allAppointmentDetails, $scope.currentProvider.uuid);
@@ -597,7 +621,7 @@ angular.module('bahmni.appointments')
             };
 
             $scope.isEditAllowed = function () {
-                return $scope.isPastAppointment ? false : (_.includes(['Requested' ,'Scheduled', 'CheckedIn'], $scope.appointment.status));
+                return $scope.isPastAppointment ? false : (_.includes(['Requested', 'Scheduled', 'CheckedIn'], $scope.appointment.status));
             };
 
             $scope.navigateToPreviousState = function () {
