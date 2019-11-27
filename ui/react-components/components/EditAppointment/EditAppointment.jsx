f7bc41d1359e4bfb247a564d980643d3a82f5583
nehajain <neha.jain216@gmail.com>
2019-11-27 13:26:08 +0530

    Neha, Siva | MOBN-880 | fixed issue related to esc in Add Appointments and Edit Appointments.

--- ui/react-components/components/EditAppointment/EditAppointment.jsx
+++ ui/react-components/components/EditAppointment/EditAppointment.jsx
@@ -515,6 +515,7 @@ const EditAppointment = props => {
             <CustomPopup style={conflictsPopup} open={true}
                          closeOnDocumentClick={false}
                          closeOnEscape={true}
+                         onClose={() => setConflicts(undefined)}
                          popupContent={<Conflicts saveAnyway={saveAppointments}
                                                   modifyInformation={() => setConflicts(undefined)}
                                                   conflicts={conflicts} service={appointmentDetails.service}/>}/>}
