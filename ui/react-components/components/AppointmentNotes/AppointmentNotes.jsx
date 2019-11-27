bca004fa7a82b2e561babb6b02e4ebd68515d3e7
vinay-ThoughtWorks <g.saivinay@thoughtworks.com>
2019-11-27 16:39:08 +0530

    Vinay, Alekhya | MOBN - 815 | Notes alignment fix

--- ui/react-components/components/AppointmentNotes/AppointmentNotes.jsx
+++ ui/react-components/components/AppointmentNotes/AppointmentNotes.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import {notes} from './AppointmentNotes.module.scss'
 
 const AppointmentNotes = (props) => (
-        <div>
+        <Fragment>
             <textarea value={props.value || ''} className={classNames(notes)} onChange={props.onChange} data-testid="notes"/>
-        </div>
+        </Fragment>
 );
 
 AppointmentNotes.propTypes = {
