0ea46b93877450b7d821c7bd4389cfcc624b3250
tbindu <tbindu@thoughtworks.com>
2019-12-03 17:11:26 +0530

    Bindu | MOBN-814 | Added maxallowed length and placeholder for notes textarea

--- ui/react-components/components/AppointmentNotes/AppointmentNotes.jsx
+++ ui/react-components/components/AppointmentNotes/AppointmentNotes.jsx
@@ -1,18 +1,27 @@
 import React, {Fragment} from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
-import {notes} from './AppointmentNotes.module.scss'
+import {notes} from './AppointmentNotes.module.scss';
+import {injectIntl} from "react-intl";
 
-const AppointmentNotes = (props) => (
-        <Fragment>
-            <textarea value={props.value || ''} className={classNames(notes)} onChange={props.onChange} data-testid="notes"/>
+const AppointmentNotes = (props) => {
+    const {intl, onChange, value} = props;
+    const placeHolder = intl.formatMessage({
+        id: 'PLACEHOLDER_APPOINTMENT_NOTES_MAX_LENGTH', defaultMessage: 'Maximum of 250 characters'
+    });
+    return (<Fragment>
+            <textarea value={value || ''} className={classNames(notes)} onChange={onChange}
+                      data-testid="notes" maxLength="250"
+                      placeholder={placeHolder}/>
         </Fragment>
-);
+    )
+};
 
 AppointmentNotes.propTypes = {
     onChange: PropTypes.func.isRequired,
-    value: PropTypes.string
+    value: PropTypes.string,
+    intl: PropTypes.object.isRequired
 };
 
-export default AppointmentNotes;
+export default injectIntl(AppointmentNotes);
 
