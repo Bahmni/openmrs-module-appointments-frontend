5dd7c718599746255e0bcdcd1c2d1e93c5642ff4
MD Dubey <mddubey409@gmail.com>
2019-12-05 12:24:28 +0530

    Dubey | AP-34 | Address review feedback

--- src/appointmentStatusHandler.js
+++ src/appointmentStatusHandler.js
@@ -12,80 +12,91 @@ Bahmni.Appointments.AppointmentStatusHandler = (function () {
         });
     };
 
+    const appointmentStatuses = function () {
+        return Bahmni.Appointments.Constants.appointmentStatuses;
+    };
+
+    const providerResponses = function () {
+        return Bahmni.Appointments.Constants.providerResponses;
+    };
+
     const isStatusRequested = function (status) {
-        return status === Bahmni.Appointments.Constants.appointmentStatuses.Requested;
+        return status === appointmentStatuses().Requested;
     };
 
     const isStatusScheduled = function (status) {
-        return status === Bahmni.Appointments.Constants.appointmentStatuses.Scheduled;
+        return status === appointmentStatuses().Scheduled;
     };
 
     const isNewAppointment = function (appointment) {
         return !appointment.uuid;
     };
 
-    const updateIfCurrentProviderInAppointment = function (statusAndProviderResponse, currentProviderUuid, appointment) {
-        const currentProviderInAppointment = _.find(statusAndProviderResponse.providers, provider => provider.uuid === currentProviderUuid);
-        if (!currentProviderInAppointment) return;
-
-        currentProviderInAppointment.response = Bahmni.Appointments.Constants.providerResponses.ACCEPTED;
+    const getStatusForAppointment = function (appointment) {
         if (isNewAppointment(appointment) || isStatusRequested(appointment.status)) {
-            statusAndProviderResponse.status = Bahmni.Appointments.Constants.appointmentStatuses.Scheduled;
+            return appointmentStatuses().Scheduled;
         } else {
-            statusAndProviderResponse.status = appointment.status;
+            return appointment.status;
         }
     };
 
-    const handleRescheduledAppointment = function (statusAndProviderResponse, appointment, currentProviderUuid) {
-        //this is an special edit
-        // in this case we don't keep the existing appointment status and responses
-        statusAndProviderResponse.status = Bahmni.Appointments.Constants.appointmentStatuses.Requested;
-        statusAndProviderResponse.providers = _.map(appointment.providers, function (provider) {
-            return {uuid: provider.uuid, response: Bahmni.Appointments.Constants.providerResponses.AWAITING}
-        });
+    const updateIfCurrentProviderInAppointment = function (statusAndProviderResponse, currentProviderUuid, appointment) {
+        const clone = _.cloneDeep(statusAndProviderResponse);
+        const isCurrentProviderInAppointment = _.some(statusAndProviderResponse.providers, provider => provider.uuid === currentProviderUuid);
+        if (!isCurrentProviderInAppointment) return clone;
 
-        const currentProviderInAppointment = _.find(statusAndProviderResponse.providers, provider => provider.uuid === currentProviderUuid);
-        if (currentProviderInAppointment) {
-            statusAndProviderResponse.status = Bahmni.Appointments.Constants.appointmentStatuses.Scheduled;
-            currentProviderInAppointment.response = Bahmni.Appointments.Constants.providerResponses.ACCEPTED;
-        }
+        clone.status = getStatusForAppointment(appointment);
+        clone.providers = _.map(clone.providers, function (provider) {
+            const response = (provider.uuid === currentProviderUuid) ?
+                providerResponses().ACCEPTED : provider.response;
+            return {uuid: provider.uuid, response: response};
+        });
+        return clone;
+    };
+
+    const updateIfRescheduled = function (statusAndProviderResponse, appointment, currentProviderUuid) {
+        // in this case we don't keep the existing appointment status and responses
+        //this is an special edit
+        const clone = _.cloneDeep(statusAndProviderResponse);
+        const isCurrentProviderInAppointment = _.some(clone.providers, provider => provider.uuid === currentProviderUuid);
+
+        clone.status = isCurrentProviderInAppointment ? appointmentStatuses().Scheduled :
+            appointmentStatuses().Requested;
+        clone.providers = _.map(clone.providers, function (provider) {
+            const response = (provider.uuid === currentProviderUuid) ?
+                providerResponses().ACCEPTED : providerResponses().AWAITING;
+            return {uuid: provider.uuid, response: response};
+        });
+        return clone;
     };
 
     const updateIfAtleastOneProviderHasAccepted = function (statusAndProviderResponse) {
         //this handles special cases like,
         //  when new providers are added to a no provider appointment
         //  when only accepted provider is removed from appointment appointment
-        const hasAtleastOneAccept = _.some(statusAndProviderResponse.providers, function (provider) {
-            return provider.response === Bahmni.Appointments.Constants.providerResponses.ACCEPTED;
+
+        const clone = _.cloneDeep(statusAndProviderResponse);
+        const hasAtleastOneAccept = _.some(clone.providers, function (provider) {
+            return provider.response === providerResponses().ACCEPTED;
         });
         if (hasAtleastOneAccept) {
-            if (isStatusRequested(statusAndProviderResponse.status)) {
-                statusAndProviderResponse.status = Bahmni.Appointments.Constants.appointmentStatuses.Scheduled;
+            if (isStatusRequested(clone.status)) {
+                clone.status = appointmentStatuses().Scheduled;
             }
         } else {
-            if (isStatusScheduled(statusAndProviderResponse.status)) {
-                statusAndProviderResponse.status = Bahmni.Appointments.Constants.appointmentStatuses.Requested;
+            if (isStatusScheduled(clone.status)) {
+                clone.status = appointmentStatuses().Requested;
             }
         }
+        return clone;
     };
-
-    const statusAndResponseForZeroProviders = function (appointment) {
-        const statusAndProviderResponse = {providers: []};
-        if (isNewAppointment(appointment) || isStatusRequested(appointment.status)) {
-            statusAndProviderResponse.status = Bahmni.Appointments.Constants.appointmentStatuses.Scheduled;
-        } else {
-            statusAndProviderResponse.status = appointment.status;
-        }
-        return statusAndProviderResponse;
-    };
-
 
     const statusAndResponseForScheduledServices = function (appointment) {
         const statusAndProviderResponse = {};
         statusAndProviderResponse.status = isNewAppointment(appointment) ?
-            Bahmni.Appointments.Constants.appointmentStatuses.Scheduled : appointment.status;
+            appointmentStatuses().Scheduled : appointment.status;
         statusAndProviderResponse.providers = _.map(appointment.providers, function (provider) {
-            return {uuid: provider.uuid, response: Bahmni.Appointments.Constants.providerResponses.ACCEPTED};
+            return {uuid: provider.uuid, response: providerResponses().ACCEPTED};
         });
         return statusAndProviderResponse;
     };
@@ -93,10 +104,10 @@ Bahmni.Appointments.AppointmentStatusHandler = (function () {
     const statusAndResponseForRequestedServices = function (appointment, existingProvidersUuids) {
         const statusAndProviderResponse = {};
         statusAndProviderResponse.status = isNewAppointment(appointment) ?
-            Bahmni.Appointments.Constants.appointmentStatuses.Requested : appointment.status;
+            appointmentStatuses().Requested : appointment.status;
 
         statusAndProviderResponse.providers = mapNewProvidersToGivenResponse(appointment, existingProvidersUuids,
-            Bahmni.Appointments.Constants.providerResponses.AWAITING);
+            providerResponses().AWAITING);
         return statusAndProviderResponse;
     };
 
@@ -105,15 +116,15 @@ Bahmni.Appointments.AppointmentStatusHandler = (function () {
             return statusAndResponseForScheduledServices(appointment);
         }
         if (_.isEmpty(appointment.providers)) {
-            return statusAndResponseForZeroProviders(appointment);
+            return {status: getStatusForAppointment(appointment), providers:[]};
         }
-        const statusAndProviderResponse = statusAndResponseForRequestedServices(appointment, existingProvidersUuids);
+        let statusAndProviderResponse = statusAndResponseForRequestedServices(appointment, existingProvidersUuids);
 
-        updateIfCurrentProviderInAppointment(statusAndProviderResponse, currentProviderUuid, appointment);
-        updateIfAtleastOneProviderHasAccepted(statusAndProviderResponse);
+        statusAndProviderResponse = updateIfCurrentProviderInAppointment(statusAndProviderResponse, currentProviderUuid, appointment);
+        statusAndProviderResponse = updateIfAtleastOneProviderHasAccepted(statusAndProviderResponse);
 
         if (isRescheduled) {
-            handleRescheduledAppointment(statusAndProviderResponse, appointment, currentProviderUuid);
+            statusAndProviderResponse = updateIfRescheduled(statusAndProviderResponse, appointment, currentProviderUuid);
         }
         return statusAndProviderResponse;
     };
