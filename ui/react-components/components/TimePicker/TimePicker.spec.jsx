65cb01f640c0313e9ca81d16b86d329fd50514a7
Sneha Bagri <bsneha@thoughtworks.com>
2019-12-06 13:45:03 +0530

    Sneha Bagri | Write test for onchange of time picker

--- ui/react-components/components/TimePicker/TimePicker.spec.jsx
+++ ui/react-components/components/TimePicker/TimePicker.spec.jsx
@@ -3,6 +3,11 @@ import AppointmentTimePicker from "./TimePicker.jsx";
 import {renderWithReactIntl} from "../../utils/TestUtil";
 import {fireEvent} from "@testing-library/react";
 
+const Wrapper = (props) => {
+  return  <div>
+        {props.children}
+    </div>
+}
 
 describe('TimePicker', () => {
 
@@ -38,5 +43,21 @@ describe('TimePicker', () => {
         fireEvent.focusIn(inputBox, {target: {value: "7:10 am"}});
 
         expect(inputBox.value).toBe("7:10 am");
+    });
+
+    it('should call onchange when time entered', async () => {
+        const placeHolderKey = "CHOOSE_TIME_PLACE_HOLDER";
+        const defaultValue = "Click to select time";
+        let onChangeSpy = jest.fn();
+        const {container, baseElement} = renderWithReactIntl(<Wrapper><AppointmentTimePicker onChange={onChangeSpy}
+                                                                                                      placeHolderTranslationKey={placeHolderKey}
+
+                                                                                                      placeHolderDefaultMessage={defaultValue}/></Wrapper>);
+        const inputBox = container.querySelector('.rc-time-picker-input');
+        fireEvent.click(inputBox)
+        const entryBox = baseElement.querySelector('.rc-time-picker-panel-input');
+        fireEvent.change(entryBox, { target: { value: "7:10 am" } })
+        expect(onChangeSpy).toHaveBeenCalledTimes(1);
+        expect(entryBox.value).toBe("7:10 am");
     });
 });
