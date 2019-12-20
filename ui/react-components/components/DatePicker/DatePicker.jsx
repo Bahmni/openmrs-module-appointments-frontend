824605e5a8dd54d334939d3c2a45f91fb5cffd50
Sneha Bagri <bsneha@thoughtworks.com>
2019-12-20 15:51:14 +0530

    Sneha Bagri, Neha Jain |MOBN-945| Datepicker should not disable a value when disabled

--- ui/react-components/components/DatePicker/DatePicker.jsx
+++ ui/react-components/components/DatePicker/DatePicker.jsx
@@ -14,7 +14,7 @@ import DateInput from './DateInput.jsx'
 const datetoMomentDate = (date) => date==='' || date===null || date === undefined ? date
     : moment(date,'MM/DD/YYYY' );
 const AppointmentDatePicker = (props) => {
-    const {minDate, value, onChange,isDisabled} = props;
+    const {minDate, value, onChange, isDisabled} = props;
     let calendarStyles =[appointmentDatePicker];
     isDisabled && calendarStyles.push(disable);
     const selectedDate = value?  value.toDate() : value;
@@ -27,14 +27,16 @@ const AppointmentDatePicker = (props) => {
         onChange(valueEntered)
     };
 
+    const dateInputValue = isDisabled? '' : (value && value!=='' ? value.format('MM/DD/YYYY') : value);
+
     return (
         <div data-testid="datePicker" className={classNames(containerStyles)}>
-           <DateInput value={value && value!=='' ? value.format('MM/DD/YYYY') : value}
+           <DateInput value={dateInputValue}
                       minDate={minDate && minDate.toDate()}
                       isDisabled={isDisabled}
                       onBlur={handleBlurOfDateInput}/>
            <div disabled={isDisabled} data-testid="datePicker-Calendar"> <DatePicker
-                selected={selectedDate}
+                selected={isDisabled? '' : selectedDate}
                 onChange={(date) =>onChange(datetoMomentDate(date))}
                 inline
                 readOnly={true}
