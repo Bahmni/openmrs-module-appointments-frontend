241f03bbe189315f6dbdeb9d6beb3439f6e0a1d9
Alekhya <alekhyayalla23@gmail.com>
2020-01-02 12:00:35 +0530

    Alekhya | MOBN-984 | Add focus lock to update successful pop up

--- ui/react-components/components/SuccessModal/UpdateSuccessModal.jsx
+++ ui/react-components/components/SuccessModal/UpdateSuccessModal.jsx
@@ -12,6 +12,7 @@ import {
 } from "./SuccessModal.module.scss";
 import {AppContext} from "../AppContext/AppContext";
 import PropTypes from "prop-types";
+import FocusLock from "react-focus-lock";
 
 const UpdateSuccessModal = (props) => {
 
@@ -19,33 +20,36 @@ const UpdateSuccessModal = (props) => {
     const {onBack} = React.useContext(AppContext);
 
     return (
-        <div className={classNames(saveModal)}>
-            <div className={classNames(saveModalCloseIcon)}>
-                <a data-testid="save-close-icon">
-                    <i className={classNames("fa", "fa-times")} onClick={onBack}/>
-                </a>
-            </div>
-            <div>
-                <h1 className={classNames(saveModalTitle)}>
-                    <FormattedMessage id={'APPOINTMENT_UPDATE_SUCCESS_TITLE'} defaultMessage={'Update Successful!'}/>
-                </h1>
-
-                <div className={classNames(saveModalBody)}>
-                    {updateSeries ?
-                        <FormattedMessage id={'APPOINTMENT_UPDATE_ALL_SUCCESS_TEXT'}
-                                          defaultMessage={'Details are successfully updated for the entire appointment series.'}/> :
-                        <FormattedMessage id={'APPOINTMENT_UPDATE_SINGLE_SUCCESS_TEXT'}
-                                          defaultMessage={'Details are successfully updated for the selected appointment.'}/>
-                    }
-                </div>
-
-                <div className={classNames(saveConfirmationFooter)}>
-                    <button className={classNames(button)} data-testid="save-close-button" onClick={onBack}>
-                        <FormattedMessage id={'APPOINTMENT_SAVE_CONFIRMATION_CLOSE'} defaultMessage={'Close'}/>
+        <FocusLock>
+            <div className={classNames(saveModal)}>
+                <div className={classNames(saveModalCloseIcon)}>
+                    <button data-testid="save-close-icon" tabIndex={2}>
+                        <i className={classNames("fa", "fa-times")} onClick={onBack}/>
                     </button>
                 </div>
+                <div>
+                    <h1 className={classNames(saveModalTitle)}>
+                        <FormattedMessage id={'APPOINTMENT_UPDATE_SUCCESS_TITLE'}
+                                          defaultMessage={'Update Successful!'}/>
+                    </h1>
+
+                    <div className={classNames(saveModalBody)}>
+                        {updateSeries ?
+                            <FormattedMessage id={'APPOINTMENT_UPDATE_ALL_SUCCESS_TEXT'}
+                                              defaultMessage={'Details are successfully updated for the entire appointment series.'}/> :
+                            <FormattedMessage id={'APPOINTMENT_UPDATE_SINGLE_SUCCESS_TEXT'}
+                                              defaultMessage={'Details are successfully updated for the selected appointment.'}/>
+                        }
+                    </div>
+
+                    <div className={classNames(saveConfirmationFooter)}>
+                        <button className={classNames(button)} data-testid="save-close-button" onClick={onBack} tabIndex={1}>
+                            <FormattedMessage id={'APPOINTMENT_SAVE_CONFIRMATION_CLOSE'} defaultMessage={'Close'}/>
+                        </button>
+                    </div>
+                </div>
             </div>
-        </div>
+        </FocusLock>
     );
 };
 
