4e68ff4548a0864c3c6dc947061d511dcc822bf2
Alekhya <alekhyayalla23@gmail.com>
2020-01-02 14:09:26 +0530

    Alekhya | MOBN-984 | Add wrapper function for focus lock

--- ui/react-components/components/UpdateConfirmationModal/UpdateConfirmationModal.jsx
+++ ui/react-components/components/UpdateConfirmationModal/UpdateConfirmationModal.jsx
@@ -10,7 +10,7 @@ import {
     updateConfirmationModalTitle
 } from "./UpdateConfirmationModal.module.scss";
 import PropTypes from "prop-types";
-import FocusLock from "react-focus-lock";
+import WithFocusLock from "../FocusLock/WithFocusLock.jsx";
 
 
 const UpdateConfirmationModal = (props) => {
@@ -18,42 +18,41 @@ const UpdateConfirmationModal = (props) => {
     const {intl, close, save, updateSeries} = props;
 
     return (
-        <FocusLock>
-            <div className={classNames(updateConfirmationModal)}>
-                <div className={classNames(updateConfirmationModalCloseIcon)}>
-                    <button data-testid="update-confirmation-close-icon" tabIndex={3}>
-                        <i className={classNames("fa", "fa-times")} onClick={() => {
-                            close();
-                        }}/>
-                    </button>
+        <div className={classNames(updateConfirmationModal)}>
+            <div className={classNames(updateConfirmationModalCloseIcon)}>
+                <button data-testid="update-confirmation-close-icon" tabIndex={3} onClick={() => {
+                    close();
+                }}>
+                    <i className={classNames("fa", "fa-times")} />
+                </button>
+            </div>
+            <div>
+                <h1 className={classNames(updateConfirmationModalTitle)}>
+                    <FormattedMessage id={'APPOINTMENT_UPDATE_CONFIRMATION_TITLE'}
+                                      defaultMessage={'Kindly Confirm'}/>
+                </h1>
+                <div className={classNames(updateConfirmationModalBody)}>
+                    {updateSeries ?
+                        <FormattedMessage id={'APPOINTMENT_UPDATE_CONFIRMATION_TEXT_RECURRING_APPOINTMENT'}
+                                          defaultMessage={'This will update the details of the entire appointment series. This cannot be reversed!'}/> :
+                        <FormattedMessage id={'APPOINTMENT_UPDATE_CONFIRMATION_TEXT_SINGLE_APPOINTMENT'}
+                                          defaultMessage={'This will update the details of the selected appointment. This cannot be reversed!'}/>
+                    }
                 </div>
                 <div>
-                    <h1 className={classNames(updateConfirmationModalTitle)}>
-                        <FormattedMessage id={'APPOINTMENT_UPDATE_CONFIRMATION_TITLE'}
-                                          defaultMessage={'Kindly Confirm'}/>
-                    </h1>
-                    <div className={classNames(updateConfirmationModalBody)}>
-                        {updateSeries ?
-                            <FormattedMessage id={'APPOINTMENT_UPDATE_CONFIRMATION_TEXT_RECURRING_APPOINTMENT'}
-                                              defaultMessage={'This will update the details of the entire appointment series. This cannot be reversed!'}/> :
-                            <FormattedMessage id={'APPOINTMENT_UPDATE_CONFIRMATION_TEXT_SINGLE_APPOINTMENT'}
-                                              defaultMessage={'This will update the details of the selected appointment. This cannot be reversed!'}/>
-                        }
-                    </div>
-                    <div>
-                        <button className={classNames(button, no)} data-testid="cancel-update-button" onClick={() => {
-                            close();
-                        }} tabIndex={1}>
-                            <FormattedMessage id={'APPOINTMENT_UPDATE_CONFIRMATION_NO'} defaultMessage={'No, go back'}/>
-                        </button>
-                        <button className={classNames(button)} data-testid="update-confirm-button" onClick={save} tabIndex={2}>
-                            <FormattedMessage id={'APPOINTMENT_UPDATE_CONFIRMATION_YES'}
-                                              defaultMessage={'Yes, I confirm'}/>
-                        </button>
-                    </div>
+                    <button className={classNames(button, no)} data-testid="cancel-update-button" onClick={() => {
+                        close();
+                    }} tabIndex={1}>
+                        <FormattedMessage id={'APPOINTMENT_UPDATE_CONFIRMATION_NO'} defaultMessage={'No, go back'}/>
+                    </button>
+                    <button className={classNames(button)} data-testid="update-confirm-button" onClick={save}
+                            tabIndex={2}>
+                        <FormattedMessage id={'APPOINTMENT_UPDATE_CONFIRMATION_YES'}
+                                          defaultMessage={'Yes, I confirm'}/>
+                    </button>
                 </div>
             </div>
-        </FocusLock>
+        </div>
     );
 };
 
@@ -64,4 +63,4 @@ UpdateConfirmationModal.propTypes = {
 };
 
 
-export default injectIntl(UpdateConfirmationModal);
+export default injectIntl(WithFocusLock(UpdateConfirmationModal));
