4edeeca86cb0436e304037e46a6e7a02dda7a0d2
pratiti <31505174+openact@users.noreply.github.com>
2020-04-03 22:54:46 +0530

    Saketh, Pratiti | Add Configurable Appoitment slots and Working hourss grid color

---
+++ ui/react-components/components/AppointmentCalender/AppointmentCalender.test.js
@@ -0,0 +1,56 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import { getByTestId, queryByTestId } from "@testing-library/dom";
+import "@testing-library/jest-dom/extend-expect";
+import AppointmentCalender from './AppointmentCalender';
+
+import data from './mockAppointmentsData.json'
+
+describe("<AppointmentCalender />", () => {
+    it("should render AppointmentCalender component", () => {
+        const {container} = render(
+            <AppointmentCalender appoinments={[]} />
+        );
+        expect(getByTestId(container, "appointment-calender")).toBeTruthy()
+    })
+
+    it("should show no appointment when empty object" ,() => {
+        const {getByText} = render(
+            <AppointmentCalender appoinments={[]} />
+        );
+        expect(getByText("No Appointments Found"))   
+    })
+
+    it("should render appointment labels", () => {
+        const {container} = render(
+            <AppointmentCalender appoinments={data} />
+        );
+        expect(container.getElementsByClassName("appointment_label").length).toEqual(5)
+    })
+
+    it("should work for configurable time slots", () => {
+        const {container, rerender} = render(
+            <AppointmentCalender appoinments={[]} hoursDiff={1}/>
+        );
+
+        expect(container.getElementsByTagName("tbody")[0].childNodes.length / 2).toEqual(24)
+
+        rerender(<AppointmentCalender appoinments={[]} hoursDiff={2}/>)
+
+        expect(container.getElementsByTagName("tbody")[0].childNodes.length / 2).toEqual(12)
+
+        rerender(<AppointmentCalender appoinments={[]} hoursDiff={3}/>)
+
+        expect(container.getElementsByTagName("tbody")[0].childNodes.length / 2).toEqual(8)
+
+    })
+
+    it("should show providers properly", () => {
+        const {container} = render(
+            <AppointmentCalender appoinments={data} hoursDiff={1}/>
+        );
+
+        expect(container.querySelector("thead>tr").childNodes.length - 1).toEqual(4)
+        
+    })
+})
---
+++ ui/react-components/stories/AppointmentCalender.stories.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import AppointmentCalender from '../components/AppointmentCalender/AppointmentCalender';
+
+import data from '../components/AppointmentCalender/mockAppointmentsData.json'
+
+export default {
+    title: 'Appointment Calender View',
+}
+
+export const Basic = () => <AppointmentCalender appoinments={data} onSelect={(a) => alert(a)}/>
