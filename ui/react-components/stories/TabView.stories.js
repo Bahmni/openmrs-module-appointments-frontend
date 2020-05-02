648f7b07bf34f62f127d02d12671fc02bd60829d
nikhilpadmawar <nikhil.padmawar@thoughtworks.com>
2020-05-02 22:50:23 +0530

    [ Nikhil ] : Appointment list page tab view

--- ui/react-components/stories/TabView.stories.js
+++ ui/react-components/stories/TabView.stories.js
@@ -2,19 +2,19 @@ import React from "react";
 import TabView from "../components/TabView/TabView";
 import { BrowserRouter as Router } from "react-router-dom";
 
-export default { title: "Tab View" };
+export default { title: "Tab View menu" };
 
 const routes = [
   {
     heading: "Summary",
     path: "/",
-    component: "Pass component here ex : <ABC/>"
+    component: "Pass component here ex : <ABC/>",
   },
   {
     heading: "Appointments List",
     path: "/list",
-    component: ""
-  }
+    component: "",
+  },
 ];
 
 export const basic = () => (
