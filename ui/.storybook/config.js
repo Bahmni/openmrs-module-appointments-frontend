17f7bf152976c16aabed380596f65a4ea8fba32f
Subham Singh <s.chauhan5505@gmail.com>
2020-05-21 15:49:06 +0530

    Provide dynamic config in story book

--- ui/.storybook/config.js
+++ ui/.storybook/config.js
@@ -1,8 +1,9 @@
 import { configure } from '@storybook/react';
 import { library, dom } from "@fortawesome/fontawesome-svg-core";
 import { faPrint,faAngleDown,faSearch,faTimes } from "@fortawesome/free-solid-svg-icons";
+import { localReactConfig} from "../local-react-config";
 
-
+localStorage.setItem("reactConfig", JSON.stringify(localReactConfig));
 library.add(faPrint,faAngleDown,faSearch,faTimes );
 dom.watch();
 configure(require.context('../react-components', true, /\.stories\.js$/), module);
