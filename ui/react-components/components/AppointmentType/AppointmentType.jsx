a98285d2c39685bd8005e5434b2e2016e0b8bb25
Carlos Rigo <carlos.rigocano@thoughtworks.com>
2020-10-26 17:31:57 +0100

    WIP Appointment UI - Separate field for Teleconsultation - pairing with @Ana Karolina

---
+++ ui/react-components/components/AppointmentType/AppointmentType.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import Label from "../Label/Label.jsx";
+ import {checkbox, checkboxContainer, container, appointmentTypeLabel} from "./AppointmentType.module.scss";
+import classNames from "classnames";
+import PropTypes from "prop-types";
+import {injectIntl} from "react-intl";
+import Checkbox from 'rc-checkbox';
+import 'rc-checkbox/assets/index.css';
+import {TELECONSULTATION_APPOINTMENT} from "../../constants";
+
+const AppointmentType = props => {
+
+    const {onChange, teleconsultation, isTeleconsultationDisabled } = props;
+
+    return (<div className={classNames(container)}>
+        <span className={classNames(appointmentTypeLabel)}><Label className={classNames(appointmentTypeLabel)} translationKey="APPOINTMENT_TYPE_LABEL"
+        defaultValue="Appointment Type"/></span>
+        <div className={classNames(checkboxContainer)} data-test-id="add-tele-consultation">    
+            <Checkbox
+                defaultChecked={false}
+                className={classNames(checkbox)}
+                id="teleconsultation-checkbox"
+                onChange={onChange}
+                checked={teleconsultation}
+                name={TELECONSULTATION_APPOINTMENT}
+                disabled={isTeleconsultationDisabled}
+            />
+            <Label forInput="teleconsultation-checkbox" translationKey="TELECONSULTATION_APPOINTMENT_LABEL"
+                defaultValue="Teleconsultation"
+                disabled={isTeleconsultationDisabled}/>
+        </div>
+    </div>)
+};
+
+AppointmentType.propTypes = {
+    onChange: PropTypes.func,
+    appointmentType: PropTypes.string,
+    isTeleconsultationDisabled: PropTypes.bool
+};
+
+export default injectIntl(AppointmentType);
+
+// Questions?
+// translationKey for PLAN_LABEL?
+// disabled when isWalkInDisabled? Shouldn't it be when isTeleconsultationDisabled? Add test
