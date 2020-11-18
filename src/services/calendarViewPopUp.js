8eff9e7e21a0f32e56c097cb4bfdb7cc3f24803d
swatigogia2020 <swati.gogia@thoughtworks.com>
2020-11-18 13:06:10 +0530

    Praveena/Swati : navigation control fix

--- src/services/calendarViewPopUp.js
+++ src/services/calendarViewPopUp.js
@@ -43,8 +43,8 @@ angular.module('bahmni.appointments')
                 };
 
                 popUpScope.openJitsiMeet = function (appointment) {
-                    var jitsiMeetingUrl = 'https://meet.jit.si/' + appointment.uuid
-                    $window.open(jitsiMeetingUrl,'_blank');
+                    //var jitsiMeetingUrl = 'https://meet.jit.si/' + appointment.uuid
+                    //$window.open(jitsiMeetingUrl,'_blank');
 
                     $window.open(Bahmni.Common.Constants.https + 
                         window.location.hostname + 
