90fc950d0d918ff79a8909b27f7978ae1937cd3b
Sneha Bagri <snehabagri90@gmail.com>
2021-07-15 10:41:07 +0530

    Sneha Bagri | Fix storybook for AppSpecialityFilter & AppointmentListSidePanelSearch

--- ui/react-components/stories/AppSpecialityFilter.stories.js
+++ ui/react-components/stories/AppSpecialityFilter.stories.js
@@ -1,8 +1,31 @@
 import React from "react";
 import AppSpecialityFilter from "../components/AppSpecialityFilter/AppSpecialityFilter";
+import { IntlProvider } from "react-intl";
+export default { title: "AppSpecilaityFilter" };
 
-export default { title: "AppointmentListSidePanelTree" };
-
+const withReactIntl = (AppSpecialityFilter) => {
+  return (props) => {
+    return (
+      <IntlProvider
+        locale="en"
+        messages={{ DROPDOWN_NO_OPTIONS_MESSAGE: "no option" }}
+      >
+        <AppSpecialityFilter {...props} />
+      </IntlProvider>
+    );
+  };
+};
+const nodes = [
+  {
+    value: "Physiotherapy OPD",
+    label: "Physiotherapy OPD",
+    children: [
+      { value: "1 session [30 min]", label: "1 session [30 min]" },
+      { value: "POP [30 min]", label: "POP [30 min]" },
+    ],
+  },
+];
+const InternationalizedAppSpecialityFilter = withReactIntl(AppSpecialityFilter);
 export const basic = () => (
-  <AppSpecialityFilter></AppSpecialityFilter>
+  <InternationalizedAppSpecialityFilter nodes={nodes} />
 );
--- ui/react-components/stories/AppointmentListSidePanelSearch.stories.js
+++ ui/react-components/stories/AppointmentListSidePanelSearch.stories.js
@@ -1,6 +1,20 @@
 import React from "react";
 import Search from "../components/AppointmentListSidePanelSearch/AppointmentListSidePanelSearch";
-
+import { IntlProvider } from "react-intl";
 export default { title: "AppointmentListSidePanelSearch" };
 
-export const basic = () => <Search></Search>;
+const withReactIntl = (Search) => {
+  return (props) => {
+    return (
+      <IntlProvider
+        locale="en"
+        messages={{ DROPDOWN_NO_OPTIONS_MESSAGE: "no option" }}
+      >
+        <Search {...props} />
+      </IntlProvider>
+    );
+  };
+};
+
+const InternaltionalizedSearch = withReactIntl(Search);
+export const basic = () => <InternaltionalizedSearch />;
