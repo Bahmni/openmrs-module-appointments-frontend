0d3070db770f61f0d33e0f6b2eb2f80c970709ae
Sneha Bagri <snehabagri90@gmail.com>
2021-07-15 11:15:13 +0530

    Sneha Bagri | Consume messages passed to withReactIntl util

--- ui/react-components/stories/util.js
+++ ui/react-components/stories/util.js
@@ -1,12 +1,10 @@
 import { IntlProvider } from "react-intl";
 import React from "react";
-export const withReactIntl = (Component) => {
+
+export const withReactIntl = (Component, messages = {}) => {
   return (props) => {
     return (
-      <IntlProvider
-        locale="en"
-        messages={{ DROPDOWN_NO_OPTIONS_MESSAGE: "no option" }}
-      >
+      <IntlProvider locale="en" messages={messages}>
         <Component {...props} />
       </IntlProvider>
     );
