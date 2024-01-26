d0e5e0d63d83eebcf78db7639dcb4fa914d3fd4e
MOHANKUMAR T <31698165+mohan-13@users.noreply.github.com>
2024-01-26 18:17:56 +0530

    BAH-3484 | Fix. Date format to be shown as dd/mmm/yyyy (#317)
    * BAH-3484 | Fix. Date format to be shown as dd/mmm/yyyy
    
    * BAH-3484 | Refactor. Rename method to isValidDate
    
    ---------
    
    Co-authored-by: parvathy00 <parvathy.babu@thoughtworks.com>

--- ui/react-components/components/AddAppointment/AddAppointment.test.jsx
+++ ui/react-components/components/AddAppointment/AddAppointment.test.jsx
@@ -168,8 +168,8 @@ describe('Add Appointment', () => {
         const dateSelectedField = container.querySelector('.react-datepicker__day--selected');
         expect(dateSelectedField.textContent).toBe(selectedDate.date().toFixed(0));
 
-        const dateInputField = getByPlaceholderText('mm/dd/yyyy');
-        expect(dateInputField.value).toBe(selectedDate.format('MM/DD/YYYY'));
+        const dateInputField = getByPlaceholderText('dd/mmm/yyyy');
+        expect(dateInputField.value).toBe(selectedDate.format('DD/MMM/YYYY'));
 
         fireEvent.click(getByText('Check and Save'));
 
@@ -413,8 +413,8 @@ describe('Add Appointment', () => {
             appointmentParams={appointmentParams}/>);
         expect(container.querySelectorAll('.rc-time-picker-input')[0].value).toBe(today.format('h:mm A').toLowerCase());
         expect(container.querySelectorAll('.rc-time-picker-input')[1].value).toBe(addTwoHoursFromNow.format('h:mm A').toLowerCase());
-        const dateInputField = getByPlaceholderText('mm/dd/yyyy');
-        expect(dateInputField.value).toBe(today.format('MM/DD/YYYY'));
+        const dateInputField = getByPlaceholderText('dd/mmm/yyyy');
+        expect(dateInputField.value).toBe(today.format('DD/MMM/YYYY'));
     });
 
     it('should populate the start date, start time and end time coming as prop for recurring appointment', function () {
@@ -432,9 +432,9 @@ describe('Add Appointment', () => {
         fireEvent.click(checkBoxService);
         expect(container.querySelectorAll('.rc-time-picker-input')[0].value).toBe(today.format('h:mm A').toLowerCase());
         expect(container.querySelectorAll('.rc-time-picker-input')[1].value).toBe(addTwoHoursFromNow.format('h:mm A').toLowerCase());
-        const dateInputField = getAllByPlaceholderText('mm/dd/yyyy')[0];
+        const dateInputField = getAllByPlaceholderText('dd/mmm/yyyy')[0];
 
-        expect(dateInputField.value).toBe(today.format('MM/DD/YYYY'));
+        expect(dateInputField.value).toBe(today.format('DD/MMM/YYYY'));
     });
 
     it('should not add second provider when maxAppointmentProvidersAllowed is 1', async () => {
@@ -528,8 +528,8 @@ describe('Add Appointment', () => {
         const dateSelectedField = container.querySelector('.react-datepicker__day--selected');
         expect(dateSelectedField.textContent).toBe(selectedDate.date().toFixed(0));
 
-        const dateInputField = getByPlaceholderText('mm/dd/yyyy');
-        expect(dateInputField.value).toBe(selectedDate.format('MM/DD/YYYY'));
+        const dateInputField = getByPlaceholderText('dd/mmm/yyyy');
+        expect(dateInputField.value).toBe(selectedDate.format('DD/MMM/YYYY'));
 
     });
     it('should fetch patient details on load if patient is present in url params', () => {
