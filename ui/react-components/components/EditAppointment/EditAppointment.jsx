4d94b6387feee808c462bca11ccbbd360565f7b4
Phanindra-tw <v.tadikonda@thoughtworks.com>
2024-03-22 15:35:43 +0530

    Phani : Fix tests

--- ui/react-components/components/EditAppointment/EditAppointment.jsx
+++ ui/react-components/components/EditAppointment/EditAppointment.jsx
@@ -629,8 +629,8 @@ const EditAppointment = props => {
             <div data-testid="recurring-plan-checkbox">
                     <div className={classNames(appointmentPlanContainer)}>
                         <ContentSwitcher selectedIndex={recurring? 1 : 0} >
-                            <Switch name="Regular" disabled={recurring}>Regular Appointment</Switch>
-                            <Switch name="Recurring" disabled={!recurring}>Recurring Appointment</Switch>
+                            <Switch name="Regular" disabled={recurring}text={intl.formatMessage({id: 'REGULAR_APPOINTMENT_LABEL', defaultMessage: "Regular Appointment"})}/>
+                            <Switch name="Recurring" disabled={!recurring}text={intl.formatMessage({id: 'RECURRING_APPOINTMENT_LABEL', defaultMessage: "Recurring Appointment"})}/>
                         </ContentSwitcher>
                     </div>
             </div>
