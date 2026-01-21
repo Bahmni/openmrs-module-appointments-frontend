dd9977d9388d92055d134412b286137bb00d146a
Subhash Yadav <99490164+subhashydv@users.noreply.github.com>
2026-01-21 11:10:31 +0530

    BAH-4392 | Update. styling for Service input dropdown

--- ui/react-components/components/AppointmentEditorCommonFieldsWrapper/AppointmentEditorCommonFieldsWrapper.jsx
+++ ui/react-components/components/AppointmentEditorCommonFieldsWrapper/AppointmentEditorCommonFieldsWrapper.jsx
@@ -1,6 +1,6 @@
 import React, {Fragment} from "react";
 import classNames from "classnames";
-import {commonFields, tableWrapper} from "./AppointmentEditorCommonFieldsWrapper.module.scss";
+import {commonFields, tableWrapper, noSpeciality} from "./AppointmentEditorCommonFieldsWrapper.module.scss";
 import PatientSearch from "../PatientSearch/PatientSearch.jsx";
 import ErrorMessage from "../ErrorMessage/ErrorMessage.jsx";
 import ServiceSearch from "../Service/ServiceSearch.jsx";
@@ -100,7 +100,7 @@ const AppointmentEditorCommonFieldsWrapper = props => {
                         </td>
                         <td/>
                     </tr>}
-                    <tr>
+                    <tr className={!isSpecialitiesEnabled(appConfig) ? classNames(noSpeciality) : ''}>
                         <td>
                             {isSpecialitiesEnabled(appConfig) ?
                                 <div data-testid="speciality-search">
