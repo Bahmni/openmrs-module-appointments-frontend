99125ad707c76de6060b7507bd6fac0ab7c69833
Lingeswaran Subramaniyam <lingeswaran.s@thoughtworks.com>
2026-06-14 08:30:03 +0530

    BAH-4755 refered to use local storage for home path

--- src/initialization.js
+++ src/initialization.js
@@ -1,25 +1,15 @@
 'use strict';
 
 angular.module('bahmni.appointments').factory('initialization',
-    ['authenticator', 'appService', 'spinner', 'configurations', '$q', '$http', '$rootScope', 'openMRSHelperService', 'openMRSAuthService',
-        function (authenticator, appService, spinner, configurations, $q, $http, $rootScope, openMRSHelperService, openMRSAuthService) {
+    ['authenticator', 'appService', 'spinner', 'configurations', '$q', '$rootScope', 'openMRSHelperService', 'openMRSAuthService',
+        function (authenticator, appService, spinner, configurations, $q, $rootScope, openMRSHelperService, openMRSAuthService) {
             return function () {
+                $rootScope.homeURL = localStorage.getItem('homeURL') || Bahmni.Appointments.Constants.homeUrl;
                 var loadConfigPromise = function () {
                     return configurations.load([]);
                 };
                 var initApp = function () {
                     return appService.initApp('appointments', {'app': true, 'extension': true});
-                };
-                var loadHomeConfig = function () {
-                    return appService.loadMandatoryConfig(Bahmni.Common.Constants.baseUrl + "home/app.json").then(
-                        function (response) {
-                            var config = response.data && response.data.config;
-                            $rootScope.homeURL = (config && config.homeURL) || Bahmni.Appointments.Constants.homeUrl;
-                        },
-                        function () {
-                            $rootScope.homeURL = Bahmni.Appointments.Constants.homeUrl;
-                        }
-                    );
                 };
                 var ensureLogin = function () {
                     return openMRSHelperService.isRunningOnOpenMRS().then(
@@ -30,7 +20,7 @@ angular.module('bahmni.appointments').factory('initialization',
                         });
                 };
                 return spinner.forPromise(
-                    ensureLogin().then(initApp).then(loadHomeConfig).then(loadConfigPromise)
+                    ensureLogin().then(initApp).then(loadConfigPromise)
                 );
             };
         }
